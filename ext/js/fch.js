c6cb952bb9fa6cce051328d733106ddfe202c47b
ivansojivarghese <ivansojivarghese@gmail.com>
2022-09-28 21:32:25 +0800

    update

--- ext/js/fch.js
+++ ext/js/fch.js
@@ -25,5 +25,5 @@ function loadUp() {
 function load_css_e() { // load CSS styles (page specific)
     c_css("#intro_sc", "height: calc(" + cH + "px - 7rem);", false, null); // set landing page to full height (exclusive of url bar on mobile/tablet devices)
     c_css("#profile_image, #intro_sc .content", "margin-top: calc((" + cH + "px - 27.5rem) / 5);", false, null); // margins are relative to the height
-    c_css("#ham_C", "margin-top: calc((" + cH + "px - 8rem) / 2);", false, null);
+    c_css("#ham_C", "margin-top: calc((" + cH + "px - 16rem) / 2);", false, null);
 }
