3bab740387f77af0d23a463822805e7975cbad65
ivansojivarghese <ivansojivarghese@gmail.com>
2022-12-24 10:45:13 +1000

    update

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -9,12 +9,51 @@ var im = { // #intro_main
         p : 0.5, // parallax scroll constant
         s : true // code execution status
     },
+    
+    sI_1 = { // stats numerals
+        a : 0, // initial   
+        n : 97, // km 
+        e : document.getElementById("sIn1"), // element
+        _L : undefined, // spaces for _L (loop) iterations
+        s : false // run (execution) status
+    },
+    sI_2 = {
+        a : 0,
+        n : 183, // hours
+        e : document.getElementById("sIn2"), 
+        _L : undefined,
+        s : false
+    },
+    sI_3 = {
+        a : 0,
+        n : 245, // cappuccinos
+        e : document.getElementById("sIn3"),
+        _L : undefined,
+        s : false
+    },
     s_L = null; // loop variable
 
 
 function sc_L() { // functions (live on scroll)
     var d = (pos.yA !== 0) ? Math.abs(pos.y - pos.yA) : 0; // obtain distance of scroll
     if (d > pos.st) { // check if scroll distance is valid (of a true scroll - prevents unwanted scrolling)
+        var b1 = getBd(sI_1.e, "top"), // get respective 'top' boundaries for each stat
+            b2 = getBd(sI_2.e, "top"),
+            b3 = getBd(sI_3.e, "top");
+
+        if (chkVL(b1) && (b1 < wH) && !sI_1.s) { // stats 1 (if within viewport visual)
+            e_Ic(sI_1, null, sI_1.n);
+            sI_1.s = true; // only execute each block once
+        }
+        if (chkVL(b2) && (b2 < wH) && !sI_2.s) { // stats 2
+            e_Ic(sI_2, null, sI_2.n);
+            sI_2.s = true;
+        }
+        if (chkVL(b3) && (b3 < wH) && !sI_3.s) { // stats 3
+            e_Ic(sI_3, null, sI_3.n);
+            sI_3.s = true;
+        }
+        /*
         if (pos.y === 0 && !pos.r && num_Ct(pos.v, op.e, true) && im.s) { // during a high-speed reverse scroll to the top (pos.y < threshold of viewport)
             im.t.classList.add("trs"); // add transitioning for 'smoothening' of effect
             im.s = false;
@@ -24,7 +63,7 @@ function sc_L() { // functions (live on scroll)
             } else if ((pos.y > 0) || num_Ct(pos.v, op.e, false) || pos.r) { // reset
                 im.s = true; // enabler
             }
-        }
+        }*/
     }
 
     // im.t.style.backgroundColor = "rgba(48, 48, 48, " + (pos.y * im.j) + ")"; // #intro_main tint opacity
