db7fe9ea3af77c63f0b2a54bbe8e99808aa0af4b
ivansojivarghese <ivansojivarghese@gmail.com>
2023-01-12 11:40:13 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -285,7 +285,7 @@ function pL() { // site parameters loop
     }
 
     if (!op.nav.b) { // check if URL bar is NOT in view
-        pg.msg.net.style.transform = "translateY(30px)";
+        pg.msg.net.style.transform = "translateY(" + barHeightTemp + "px)";
     }
 
     if (op.n === false && !op.nc) { // if loss of network connection (internet)
