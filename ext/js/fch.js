346a8aad1d26d3df7f9dc3aa257717641ffa86d8
ivansojivarghese <ivansojivarghese@gmail.com>
2023-01-13 11:24:03 +0800

    update

--- ext/js/fch.js
+++ ext/js/fch.js
@@ -98,14 +98,14 @@ function load_eN() { // load, after cookie acceptance (page specific)
 }
 
 function scrollArrowIterate() {
-    el.lk3b.style.top = "2rem";
-    el.lk3b.style.height = "calc(100% - 2rem)";
+    el.lk3b.style.top = "1rem";
+    el.lk3b.style.height = "calc(100% - 2.5rem)";
     setTimeout(function() {
         el.lk3b.style.top = "auto";
-        el.lk3b.style.bottom = "2rem";
+        el.lk3b.style.bottom = "1rem";
         el.lk3b.style.height = 0;
         setTimeout(function() {
             scrollArrowIterate();
-        }, op.t);
-    }, op.t);
+        }, op.te);
+    }, op.te);
 }
