a921c5c4dcd413d04d8b3299fb484dc11fcf6e51
ivansojivarghese <ivansojivarghese@gmail.com>
2023-01-18 14:29:00 +0800

    Update ld.js

--- js/ld.js
+++ js/ld.js
@@ -9,7 +9,7 @@ var disp = document.getElementById("display_sc"), // display
     rL = { // page/resource loader
         f : { // - favicon/logo
             el : document.getElementsByClassName("logo"), // element
-            u : '../svg/logo/favicon_b.svg' // relative path
+            u : 'logo/favicon.png' // relative path
         },
         w_f : { // - wifi off
             el : document.getElementsByClassName("wifi_off_img"),
