0ebcf43e8b0c4cade54a2bfd87beb5a2c20b419d
ivansojivarghese <ivansojivarghese@gmail.com>
2023-03-07 12:47:46 +0800

    Update ld.js

--- js/ld.js
+++ js/ld.js
@@ -122,7 +122,7 @@ function docRead() {
                         // web connection
 
                 }
-                if ((rdS(Rd) && !rL.e3 && !op.ne.w) || (rdS(Rd) && op.ne.w && !op.ne.s)) { // when favicon has loaded (or not)
+                if ((rdS(Rd) && !rL.e3 && !op.ne.w) || (rdS(Rd) && op.ne.w && op.ne.s)) { // when favicon has loaded (or not)
                     rL.e3 = true; 
                     if (op.c.a) { // only if first-time access
                         e_Fd(rL.g, false); // show logo
