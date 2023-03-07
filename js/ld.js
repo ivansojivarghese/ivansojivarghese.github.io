45d0d9670b7f2c5f273eee07832f679bb2c561ca
ivansojivarghese <ivansojivarghese@gmail.com>
2023-03-08 01:02:44 +0800

    Update ld.js

--- js/ld.js
+++ js/ld.js
@@ -176,7 +176,7 @@ function docRead() {
                     }
                 }
 
-            } else if (rdS(Rd) && op.ne.w && op.ne.s) { // if network slow (with background processes loaded)
+            } else if (rdS(Rd) && op.ne.w && op.ne.s && op.n) { // if network slow (with background processes loaded)
 
                 rL.xep.innerHTML = op.ne.s.toFixed(1) + " mbps";
 
@@ -196,6 +196,15 @@ function docRead() {
 
                 rL.e5 = false;
                 */
+            } else if (op.n === false) { // if network offine
+                rL.dt.classList.add("aniM-f"); // stop animation on 'load_dot'
+                rL.n.classList.add("wifi_off_img");
+                e_Fd(rL.n, false); 
+                
+                if (isFontAvailable("Poppins") && isFontAvailable("Raleway")) { // check if fonts are downloaded
+                    rL.xc.innerHTML = "offline";
+                    e_Fd(rL.x, false); // show message when internet not connected
+                }
             } else if (rdS(Rd) && !rL.e5 && rL.y) { // show webpage once all processes (requests, etc.) are complete
 
                 rL.s = true; // set load status to true
@@ -214,15 +223,6 @@ function docRead() {
                 clearInterval(_Ld); // stop ready-check loop
             } else if (op.ne.s >= op.ne.h && rL.e5) {
                 rL.e5 = false; // fix - if code block had executed unwantedly, but network speed remains optimal.
-            } else if (op.n === false) { // if network offline
-                rL.dt.classList.add("aniM-f"); // stop animation on 'load_dot'
-                rL.n.classList.add("wifi_off_img");
-                e_Fd(rL.n, false); 
-                
-                if (isFontAvailable("Poppins") && isFontAvailable("Raleway")) { // check if fonts are downloaded
-                    rL.xc.innerHTML = "offline";
-                    e_Fd(rL.x, false); // show message when internet not connected
-                }
             }
         break;
     }
