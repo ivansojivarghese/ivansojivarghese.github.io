78dfd01ee850087fea6cc0f5e13a46ad7e8a0800
ivansojivarghese <ivansojivarghese@gmail.com>
2023-03-28 12:12:45 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -948,12 +948,14 @@ window.addEventListener("resize", documentHeight);
 documentHeight();*/
 
 window.addEventListener("resize", function() {
-    if (wH !== window.innerHeight && wD !== window.innerWidth && window.innerHeight !== wD && window.innerWidth !== wH) { // check for change in width/height values before proceeding
-        wH = window.innerHeight; // update on window size variables
-        wD = window.innerWidth; 
-        cH = document.documentElement.clientHeight;
-        reL(); // reload page
-    }
+    setTimeout(function() {
+        if (wH !== window.innerHeight && wD !== window.innerWidth && window.innerHeight !== wD && window.innerWidth !== wH) { // check for change in width/height values before proceeding
+            wH = window.innerHeight; // update on window size variables
+            wD = window.innerWidth; 
+            cH = document.documentElement.clientHeight;
+            reL(); // reload page
+        }
+    }, 10);
 });
 
 window.addEventListener("orientationchange", function() {
