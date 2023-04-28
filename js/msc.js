4f4031dc7df72e4cc43f1e3f46cef0aa41d7ac20
ivansojivarghese <ivansojivarghese@gmail.com>
2023-04-28 14:15:25 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -851,7 +851,7 @@ function removeAnomalies(c_ar, a, m, s, r) { // remove further anomalies (possib
         }
     }
     for (r = 0, z = 0; r <= iprItv.d.length - 1; r++) {
-        if (iprItv.d[r] <= iprItv.v[0] || (iprItv.d[r] >= iprItv.v[1] && iprItv.d[r] <= iprItv.v[2])) { // if indexes at extreme endpoints
+        if (iprItv.d[r] <= iprItv.v[0] || (iprItv.d[r] >= iprItv.v[1] && iprItv.d[r] <= iprItv.v[2])) { // if indexes at extreme segment-thirds
             iprItv.m[z] = iprItv.d[r]; // count in for removal
             z++;
         }
