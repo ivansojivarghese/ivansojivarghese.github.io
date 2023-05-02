758a7a8b1ddaeafd667add7895087d6060b561cc
ivansojivarghese <ivansojivarghese@gmail.com>
2023-05-02 12:49:37 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -867,6 +867,8 @@ function removeAnomalies(c_ar, a, m, s, r) { // remove further anomalies (possib
                 // v1, v2, v3 distribution - ensure every segment (esp. v1/v3) HAS 1 ELEMENT AT LEAST
             // 
 
+        // MODIFY iprItv.mL/iprItv.mH intervals here!
+
         var mL = iprItv.mL === null ? 0 : iprItv.mL + 1, // attain high/low thresholds (if applicable)
             mH = iprItv.mH === null ? ipr.length - 1 : iprItv.mH;
         for (c = mL, d = 0; c <= mH; c++) {
