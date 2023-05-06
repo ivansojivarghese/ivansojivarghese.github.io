e10d9f6c4ab6c4e77764ff391ad5a113a36fd873
ivansojivarghese <ivansojivarghese@gmail.com>
2023-05-06 21:03:14 +0800

    Update ld.js

--- js/ld.js
+++ js/ld.js
@@ -388,7 +388,8 @@ function docRead() {
                 // show the page
                     // set scroll 
 
-                clearInterval(_Ld); // stop ready-check loop
+                // clearInterval(_Ld); // stop ready-check loop
+
             } else if (op.ne.s >= op.ne.h && rL.e5) {
                 rL.e5 = false; // fix - if code block had executed unwantedly, but network speed remains optimal.
             }
