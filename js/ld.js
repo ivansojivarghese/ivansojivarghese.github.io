9d46644abe5fbe20726b5def18f14363cef7417f
ivansojivarghese <ivansojivarghese@gmail.com>
2023-05-06 21:10:32 +0800

    Update ld.js

--- js/ld.js
+++ js/ld.js
@@ -352,7 +352,7 @@ function docRead() {
                 } 
             } else if (rdS(Rd) && !rL.e5 && rL.y && loadS_res(res_ar)) { // show webpage once all processes (requests, etc.) are complete
 
-                rL.s = true; // set load status to true
+                // rL.s = true; // set load status to true
 
                 if ((getCookie("networkReload") === "true")) {
                     setCookie("networkReload", null, -1); // delete the cookie (since network is of normal speed, loaded normally)
