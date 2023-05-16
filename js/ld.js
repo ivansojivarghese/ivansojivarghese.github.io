ae926b76d78a84d1704ddb8371f6da2e3c7e4524
ivansojivarghese <ivansojivarghese@gmail.com>
2023-05-16 09:29:46 +0800

    update

--- js/ld.js
+++ js/ld.js
@@ -603,6 +603,7 @@ function docRead() {
             } else {
                 pg.sc.m.style.display = "none"; // if dev error (devtools open)
                 pg.sc.m.remove(); // remove from DOM
+                // empty(document); // empty the DOM
             }
         break;
     }
