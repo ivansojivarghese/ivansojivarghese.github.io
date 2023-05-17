7c8d43c88461e3a86df16766cff93d5e687cad53
ivansojivarghese <ivansojivarghese@gmail.com>
2023-05-17 17:04:15 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -270,8 +270,8 @@ op = {
     },
     L : null // loop variable
 };
-op.zoomDefault = (op.zoom !== dev.z) ? true : false; // set zoom default status
-
+// op.zoomDefault = (op.zoom !== dev.z) ? true : false; // set zoom default status
+op.zoomDefault = approxNum(op.zoom, dev.z) ? true : false; // set zoom default status
 
 const checkOnlineStatus_abort = new AbortController(); // respective abortion functions
 const cos_signal = checkOnlineStatus_abort.signal;
