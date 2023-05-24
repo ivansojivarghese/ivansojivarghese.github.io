81dfad588a97c8cc79f60f2818f0e8f4c29e9fea
ivansojivarghese <ivansojivarghese@gmail.com>
2023-05-24 11:15:06 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -811,21 +811,22 @@ function pL() { // site parameters loop
     // pg.msg.net_t.innerHTML = window.innerHeight + " " + document.documentElement.clientHeight + " " + barHeight;
 
     if ((op.n === false && !op.nc) || (op.n && op.nc)) { // if change in network connection (internet)
-        op.nc = true; // network changed
-        
-        pg.msg.net_p.classList.add("negate"); // set color
-        pg.msg.net_i.classList.add("wifi_off_w_img"); // set content
-        pg.msg.net_t.innerHTML = "offline";
+        if (!op.nc) {
+            op.nc = true; // network changed
+            // offline
+            pg.msg.net_p.classList.add("negate"); // set color
+            pg.msg.net_i.classList.add("wifi_off_w_img"); // set content
+            pg.msg.net_t.innerHTML = "offline";
 
-        if (!pg.msg.c) {
-            msg_toggle(pg.msg.net, null, true, true, true);
+            if (!pg.msg.c) {
+                msg_toggle(pg.msg.net, null, true, true, true);
+            }
+        } else {
+            console.log("online");
         }
 
         // check for other messages, then open up
 
-    } else if (op.nc && op.n) {
-
-        console.log("online");
     }
 
     // console.log(op.ne.s + ", " + op.ne.w + ", " + op.nc);
