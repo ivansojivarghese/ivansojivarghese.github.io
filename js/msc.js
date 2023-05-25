0c6cbc337162144415438cab93edce247f0a88d9
ivansojivarghese <ivansojivarghese@gmail.com>
2023-05-25 21:26:13 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -814,6 +814,10 @@ function pL() { // site parameters loop
         }
     }
 
+    if (rL.i && !eR.s && !pg.msg.k && !pg.msg.c && !pg.cond.a && !hm.s && getCookie("testCookie") && !getCookie("cookiesAccepted")) { // if cookies have NOT been accepted by user
+        msg_toggle(pg.msg.ckA, null, true, true, null); // show cookie-acceptance message
+    }
+
     if (((!navigator.cookieEnabled && (!navigator.cookieEnabled || !getCookie("testCookie"))) || navigator.cookieEnabled && !getCookie("testCookie")) && op.c.e && !eR.s && rL.i) { // if cookies are disabled
         console.log("cookies deleted");
         
