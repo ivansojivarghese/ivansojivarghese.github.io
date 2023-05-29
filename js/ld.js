9fbfdbb622a93e35440693e98b11e1ba676dfaf8
ivansojivarghese <ivansojivarghese@gmail.com>
2023-05-29 10:47:11 +0800

    Update ld.js

--- js/ld.js
+++ js/ld.js
@@ -1032,7 +1032,7 @@ function errorCheck() { // check for errors
         eR.h = "sp";
     } else if (op.zoomDefault && !vw.z_L && (op.sys !== "iOS" && op.sys !== "Android")) { // if viewport zoom not defaulted (100%)
         eR.h = "z";
-    } else if (vw.z_S || (vw.mB_L && (op.sys !== "iOS" && op.sys !== "Android"))) { // if viewport size is too small
+    } else if (vw.z_S || (vw.mB_L && (op.sys !== "iOS" && op.sys !== "Android"))) { // if viewport size is too small (or incompatible)
         eR.h = "vs";
     } else if (vw.z_L) { // if viewport size is too large
         eR.h = "vL";
