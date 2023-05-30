9673b6c7247bb8816226071ee32a07e5e4cbc120
ivansojivarghese <ivansojivarghese@gmail.com>
2023-05-30 12:04:12 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -896,7 +896,7 @@ function pL() { // site parameters loop
         }
     }
 
-    if (op.Ld.s !== null && ) { // check for possible screen/window/tab splitting by user
+    if (op.Ld.s !== null && !op.oR) { // check for possible screen/window/tab splitting by user
         checkSplitScreen();
     }
 
