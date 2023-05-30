c935799d33caf25aa005de2cde4ade7cbde50ba4
ivansojivarghese <ivansojivarghese@gmail.com>
2023-05-30 14:03:49 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -302,6 +302,7 @@ op = {
     oR : false, // recent orientation change?
     wR : false, // window resize change?
     oRa : [], // orientation change array
+    wRa : [], // window resize change array
     aP : 5, // approximator value
     t : 200, // transition duration - default (in ms.)
     te : 500, // transition duration (extended)
@@ -1914,15 +1915,15 @@ window.addEventListener("resize", function(e) {
     } else {
         console.log("just resizing");
 
-        var id = op.oRa.length; // set id to orientation
-        op.oRa[id] = true;
+        var id = op.wRa.length; // set id to orientation
+        op.wRa[id] = true;
 
         scr_t(false, null); // disable scrolling
         op.s = true;
-        op.oR = true;
+        op.wR = true;
         setTimeout(function() {
-            if ((op.oRa.length - 1) === id) {
-                op.oR = false;
+            if ((op.wRa.length - 1) === id) {
+                op.wR = false;
             }
         }, op.te);
 
