f89730e2deb278fc14c401bc2d7a6bee184ab9bc
ivansojivarghese <ivansojivarghese@gmail.com>
2023-05-31 15:03:14 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -1807,7 +1807,7 @@ function checkSplitScreen() {
                     return true;
                 }
 
-            } else if (op.sp && !(bTop > window.screen.availHeight || bBottom > window.screen.availHeight || bLeft > window.screen.availWidth || bRight > window.screen.availWidth) && !((((bTop / window.screen.availHeight) * 100) > dev.sC_a[0]) || (((bBottom / window.screen.availHeight) * 100) < dev.sC_a[1]) || (((bLeft / window.screen.availWidth) * 100) > dev.sC_a[0]) || (((bRight / window.screen.availWidth) * 100) < dev.sC_a[1]))) { // no splitting, no error
+            } else if (op.oR && op.sp && !(bTop > window.screen.availHeight || bBottom > window.screen.availHeight || bLeft > window.screen.availWidth || bRight > window.screen.availWidth) && !((((bTop / window.screen.availHeight) * 100) > dev.sC_a[0]) || (((bBottom / window.screen.availHeight) * 100) < dev.sC_a[1]) || (((bLeft / window.screen.availWidth) * 100) > dev.sC_a[0]) || (((bRight / window.screen.availWidth) * 100) < dev.sC_a[1]))) { // no splitting, no error
                             
                 eR.sp.classList.add("z_O");
                 setTimeout(function() {
