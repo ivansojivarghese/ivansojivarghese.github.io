1de1f72da0ab82efb784e17ada45222fb369ce1d
ivansojivarghese <ivansojivarghese@gmail.com>
2023-06-02 15:28:00 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -942,15 +942,6 @@ function pL() { // site parameters loop
                     op.nc = true;
                 }
 
-                ///*
-                /*
-                if (pg.msg.net_i.classList.contains("wifi_w_img") || pg.msg.net_i.classList.contains("wifi_off_w_img") || pg.msg.net_i.classList.contains("cookies_w_img")) {
-                    pg.msg.net_i.classList.remove("wifi_w_img"); 
-                    pg.msg.net_i.classList.remove("wifi_off_w_img"); 
-                    pg.msg.net_i.classList.remove("cookies_w_img"); // remove 
-                }*/
-                //*?
-
                 if (pg.msg.net_p.classList.contains("predicate") || pg.msg.net_p.classList.contains("balanced")) {
                     pg.msg.net_p.classList.remove("predicate"); 
                     pg.msg.net_p.classList.remove("balanced"); 
@@ -971,12 +962,15 @@ function pL() { // site parameters loop
                 msg_toggle(pg.msg.net, null, false, true, true);
 
                 setTimeout(function() {
-                    if (pg.msg.net_p.classList.contains("predicate")) {
+                    if (pg.msg.net_p.classList.contains("predicate") || pg.msg.net_p.classList.contains("balanced")) {
                         pg.msg.net_p.classList.remove("predicate"); 
+                        pg.msg.net_p.classList.remove("balanced"); 
                     }
                     pg.msg.net_p.classList.add("negate"); // set color
-                    if (pg.msg.net_i.classList.contains("wifi_w_img")) {
+                    if (pg.msg.net_i.classList.contains("wifi_w_img") || pg.msg.net_i.classList.contains("cookies_w_img") || pg.msg.net_i.classList.contains("info_w_img")) {
                         pg.msg.net_i.classList.remove("wifi_w_img"); 
+                        pg.msg.net_i.classList.remove("cookies_w_img");
+                        pg.msg.net_i.classList.remove("info_w_img")
                     }
                     pg.msg.net_i.classList.add("wifi_off_w_img"); // set content
                     pg.msg.net_t.innerHTML = "offline";
