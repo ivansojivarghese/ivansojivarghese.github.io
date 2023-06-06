1d4de74859997c317d0483aaa0601199f15b239a
ivansojivarghese <ivansojivarghese@gmail.com>
2023-06-06 13:43:07 +1000

    Update scr.js

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -75,7 +75,7 @@ function sc_L() { // functions (live on scroll)
             }
         }*/
     }
-
+    /*
     if (rL && rL.i && op.c.u && !eR.s && !disp.classList.contains("d_n") && !disp.classList.contains("z_O")) { // display error fixing
         setTimeout(function() {
             var p = im.el.getBoundingClientRect(),
@@ -85,7 +85,7 @@ function sc_L() { // functions (live on scroll)
                 // reL();
             }
         }, 10); 
-    }
+    }*/
 
     // im.t.style.backgroundColor = "rgba(48, 48, 48, " + (pos.y * im.j) + ")"; // #intro_main tint opacity
     /*
