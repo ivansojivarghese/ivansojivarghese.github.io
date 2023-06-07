ed63c9c47ab794fa26aac7b59ab243750ccad35b
ivansojivarghese <ivansojivarghese@gmail.com>
2023-06-07 10:00:24 +1000

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -884,7 +884,9 @@ function pL() { // site parameters loop
                     pg.msg.ce = false;
                 }
                 op.c.e = true;
-                pg.msg.c = false;
+                if (!getCookie("testCookie")) {
+                    pg.msg.c = false;
+                }
             }, op.t);
         }
     }
