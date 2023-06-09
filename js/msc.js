267bb7b16a49119cee9a801a9525bfe0a3bfee73
ivansojivarghese <ivansojivarghese@gmail.com>
2023-06-09 12:16:58 +1000

    update

--- js/msc.js
+++ js/msc.js
@@ -158,12 +158,12 @@ window.addEventListener("load", function() { // 2 - full load
 }); 
 
 /////////////////////////////////////////////////////////
-
+/*
 const scroller = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true 
 });
-
+*/
 /////////////////////////////////////////////////////////
 
 /*
