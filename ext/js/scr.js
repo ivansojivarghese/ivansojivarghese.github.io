e17c3368fc4e56b9d4bdcbc6715e3cf7f6633aa5
ivansojivarghese <ivansojivarghese@gmail.com>
2023-06-20 11:38:11 +0800

    Update scr.js

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -116,7 +116,7 @@ function sc_L() { // functions (live on scroll)
 
         if (b.pL3.bottom < (0 - b.pL3.height)) { // parallax arrow 3
             im.s2 = true;
-            
+            c_rep(im.Lp3, "d_i", "d_n");
         } else if (b.pL3.bottom > (0 - b.pL3.height) && im.s2) {
             im.s2 = false;
             
