0bc80c4ec1b6ae910b47e58fb277f889a7ff76e5
ivansojivarghese <ivansojivarghese@gmail.com>
2023-06-23 09:11:27 +0800

    Update fch.js

--- ext/js/fch.js
+++ ext/js/fch.js
@@ -365,7 +365,6 @@ function bgCirclesMove(e) { // live gyro-based movement of bg. circles
         el.bgC[i].style.transform = "translate(" + (op.fN * y * 3) + "px, " + (op.fN * x * 3) + "px)";
     }*/
 
-    el.bgC[2].style.transform = "translate(" + (op.fN * y * 1) + "px, " + (op.fN * x * 1) + "px)"; // circle 2
     el.bgC[3].style.transform = "translate(" + (op.fN * y * 2) + "px, " + (op.fN * x * 2) + "px)"; // circle 3
 
 }
