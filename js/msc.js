8ddd8265a615c91b3f8e5a6997ed840ac4661973
ivansojivarghese <ivansojivarghese@gmail.com>
2023-06-28 13:39:10 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -1097,7 +1097,7 @@ function pL() { // site parameters loop
     ////////////////////////////////////////
 
     focus_Check = checkFocus();
-    if (!eR.s && focus_Check && !op.fC && rL && rL.i && !developer) {
+    if (!eR.s && focus_Check && !op.fC && rL && rL.i) {
         scr_t(false, null); // disable scrolling
         op.fC = true;
         op.s = true;
