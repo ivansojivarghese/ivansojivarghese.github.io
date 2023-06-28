8799954ae0170e89a15fe6e038d7aebd61e9dbfe
ivansojivarghese <ivansojivarghese@gmail.com>
2023-06-28 16:35:13 +0800

    Update os.js

--- js/os.js
+++ js/os.js
@@ -32,6 +32,8 @@ function osCheck() {
     }
 }
 
+// REFERENCED FROM KIRUPA.COM @https://www.kirupa.com/html5/check_if_you_are_on_a_touch_enabled_device.htm 
+
 function isTouchSupported() {
     var msTouchEnabled = window.navigator.msMaxTouchPoints;
     var generalTouchEnabled = "ontouchstart" in document.createElement("div");
