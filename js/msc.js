1249271a1c5456c0617d1eac628740fdaad9e319
ivansojivarghese <ivansojivarghese@gmail.com>
2023-06-28 13:43:40 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -263,7 +263,7 @@ translate_Check = checkTranslation();
 var focus_Check = false;
 
 function checkFocus() {
-    return !document.hidden && !document.hasFocus; // if document visible, but with NO focus
+    return !document.hidden && !document.hasFocus(); // if document visible, but with NO focus
 }
 
 focus_Check = checkFocus();
