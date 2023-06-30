09db75eeaab781cdf6beaa12153f2767db87d8ca
ivansojivarghese <ivansojivarghese@gmail.com>
2023-06-30 14:49:13 +0800

    Update scr.js

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -98,11 +98,210 @@ function sc_L() { // functions (live on scroll)
         e_Fd(im.Lp2, false); 
     }
 
+    im.L.style.transform = "translateY(" + (pos.y * (-1 * im.sM)) + "px)"; // #lead_sc
+    im.b5.style.transform = "translateY(" + (pos.y * -0.75) + "px)"; // bg-circle 5
+
+    if (b.Lp.top < 0 && !im.s7) { // #lead_point
+        im.s7 = true;
+        e_Fd(im.Lp1, true); // hide arrow 1
+    } else if (b.Lp.top > 0 && im.s7) {
+        im.s7 = false;
+        e_Fd(im.Lp1, false); // show arrow 1
+    }
+
+    if (b.Lps.top < 0 && !im.s8) { // #lead_point_2
+        im.s8 = true;
+        e_Fd(im.Lp2, true); // hide arrow 2
+    } else if (b.Lps.top > 0 && im.s8) {
+        im.s8 = false;
+        e_Fd(im.Lp2, false); // show arrow 2
+    }
+
+    if ((pos.r && !im.s5) || (!pos.r && im.s4) || (pos.r && im.s5)) { // downward scroll (plus, msc. conditions)
+
+        if (!im.s5 || !pos.aTs) {
+
+            if (b.e.top < aH) {
+
+                im.Lp1.style.transform = "translateY(" + (pos.y * 0.45) + "px) rotate(90deg)"; // parallax arrow 1
+                im.Lp2.style.transform = "translateY(" + (pos.y * 0.8) + "px) rotate(90deg)"; // parallax arrow 2
+                im.s6 = false;
+            }
+
+            if (b.L.bottom < aH) {
+                var r = aH - b.L.bottom;
+                im.Lp3.style.transform = "translateX(" + ((r / 2) * 0.5) + "px) translateY(" + (r * -0.4) + "px)"; // parallax arrow 3
+                im.Lp4.style.transform = "translateY(" + (r * -0.4) + "px)"; // parallax arrow 4 (hidden)
+            } else {
+                im.Lp3.style.transform = "translateX(-2rem)"; // parallax arrow 3
+            }
+        } else {
+            if (pos.r && !im.s4) {
+                im.p = op.d.getTime(); // get time of change
+                im.s4 = true;
+                setTimeout(function() {
+                    im.s4 = false;
+                }, op.t);
+            } else if (pos.r && im.s4) {
+                if ((op.d.getTime() - im.p) > op.t) {
+                    im.s5 = false; // confirmed
+                    im.s4 = false;
+                }
+            }
+        }
+    }  
+    
+    if ((!pos.r || (pos.r && im.s4)) && !pos.aT) { // upward scroll
+
+        if (im.s5 && !pos.aTs) { // /*(!pos.aTs || !pos.aT */ ) {
+            if (b.e.top < aH) {
+                im.Lp1.style.transform = "translateY(" + (pos.y * 0.45) + "px) rotate(-90deg)"; // parallax arrow 1
+                im.Lp2.style.transform = "translateY(" + (pos.y * 0.8) + "px) rotate(-90deg)"; // parallax arrow 2
+                im.s6 = true; 
+            }
+
+            if (b.L.bottom < aH) {
+                var r = aH - b.L.bottom;
+                im.Lp3.style.transform = "translateX(" + ((r / 2) * 0.5) + "px) translateY(" + (r * -0.4) + "px) rotate(180deg)"; // parallax arrow 3
+                im.Lp4.style.transform = "translateY(" + (r * -0.4) + "px)"; // parallax arrow 4 (hidden)
+            } else {
+                im.Lp3.style.transform = "translateX(-2rem) rotate(180deg)"; // parallax arrow 3
+            }
+        } else {
+            if (!pos.r && !im.s4) {
+                im.p = op.d.getTime(); // get time of change
+                im.s4 = true;
+                setTimeout(function() {
+                    im.s4 = false;
+                }, op.t);
+            } else if (!pos.r && im.s4) {
+                if ((op.d.getTime() - im.p) > op.t) {
+                    im.s5 = true; // confirmed
+                    im.s4 = false;
+                }
+            }
+        }
+    } else if (pos.aT && im.s5) {
+        im.s5 = false;
+        im.s4 = false;
+    }
+
+    if (b.pL3.bottom < (0 - b.pL3.height)) { // parallax arrow 3
+        im.s2 = true;
+        c_rep(im.Lp3, "d_i", "d_n");
+    } else if (b.pL4.bottom > (0 - b.pL4.height) && im.s2) {
+        im.s2 = false;
+        c_rep(im.Lp3, "d_n", "d_i");
+    }
+
+    if (b.Lpf.top <= ld.rf) { // c_Info header
+        ld.x2 = true;
+        if (!ld.x) {
+            ld.x = true;
+            ld.rfd = pos.y;
+        }
+        var m = (pos.y - ld.rfd) * (im.sM / (ld.rf - (ld.rfe * (1 - im.sM)))), // modifier
+            t = (-1 * (((pos.y * (1 + m)) - ld.rfd) * ((op.fN * 3.5) / (ld.rf - ld.rfe)))),
+            p = 1 - ((t) / (op.fN * -3.5));
+        if (t >= op.fN * -3.5) {
+            im.Lpf.style.transform = "translateY(" + t + "px)";
+            im.Lph.style.opacity = p;
+        } else if (t < op.fN * -3.5) {
+            im.Lpf.style.transform = "translateY(" + (op.fN * -3.5) + "px)";
+            im.Lph.style.opacity = "0";
+        }
+    } else if (ld.x2) {
+        ld.x2 = false;
+        im.Lpf.style.transform = "translateY(0px)";
+        im.Lph.style.opacity = "1";
+    }
+
+    ld.cs[0].style.transform = "translateY(" + (pos.y * -0.1) + "px)"; // c_Info 1
+    ld.cs[1].style.transform = "translateY(" + (pos.y * -0.15) + "px)"; // c_Info 2
+    ld.cs[2].style.transform = "translateY(" + (pos.y * -0.2) + "px)"; // c_Info 3
+    ld.cs[3].style.transform = "translateY(" + (pos.y * -0.25) + "px)"; // c_Info 4
+    ld.cs[4].style.transform = "translateY(" + (pos.y * -0.3) + "px)"; // c_Info 5
+
+    for (j = 0; j <= ld.cs.length - 1; j++) { // c_Info h5
+        if (ld.cs[j - 1]) {
+            if (b["csI" + (j + 1)].top < b["csI" + (j)].bottom) { // if 2 h5 elements intersect
+                ld.x3 = true;
+                break;
+            } else {
+                ld.x3 = false;
+            }
+        }
+    }
+
+    if (ld.x3) { // hide c_Info
+        ld.csm.classList.add("o-img");
+
+        ld.csa[0].setAttribute("onclick", "event.preventDefault()"); // 'photos'
+        ld.csa[0].removeAttribute("href");
+        ld.csa[0].classList.add("u-d");
+
+        ld.csa[1].setAttribute("onclick", "event.preventDefault()"); // 'projects'
+        ld.csa[1].removeAttribute("href");
+        ld.csa[1].classList.add("u-d");
+    } else { // show
+        ld.csm.classList.remove("o-img");
+
+        ld.csa[0].setAttribute("onclick", "location.href='https://ivansojivarghese.github.io/clicks'");
+        ld.csa[0].setAttribute("href", "javascript:void(0)");
+        ld.csa[0].classList.remove("u-d");
+
+        ld.csa[1].setAttribute("onclick", "location.href='https://ivansojivarghese.github.io/code'");
+        ld.csa[1].setAttribute("href", "javascript:void(0)");
+        ld.csa[1].classList.remove("u-d");
+    }
+
+    // q_Info
+
+    if (b.pb.bottom > aH && b.Lps.top > aH) {
+        if (checkScrollDir(pos.a)) { // scrolling down
+            ld.qis[1].style.transform = "translateY(calc(0.5rem * " + (pos.s) + "))";
+            ld.qis[2].style.transform = "translateY(calc(1rem * " + (pos.s) + "))";
+            ld.qis[3].style.transform = "translateY(calc(1.5rem * " + (pos.s) + "))";
+            ld.qis[4].style.transform = "translateY(calc(2rem * " + (pos.s) + "))";
+            ld.qis[5].style.transform = "translateY(calc(2.5rem * " + (pos.s) + "))";
+            ld.qis[6].style.transform = "translateY(calc(3rem * " + (pos.s) + "))";
+            ld.qis[7].style.transform = "translateY(calc(3.5rem * " + (pos.s) + "))";
+        } 
+    } else {
+        if (checkScrollDir(pos.a)) { // scrolling down
+            var _L = ld.qis.length - 1;
+            for (j = 0; j <= _L; j++) {
+                ld.qis[j].style.transform = "translateY(0px)";
+            }
+        } 
+    }
+
+    if (b.pb.bottom < 0 && b.Lps.top < 0) {
+        if (!checkScrollDir(pos.a)) { // scrolling up
+            ld.qis[0].style.transform = "translateY(calc(-3.5rem * " + (pos.s) + "))";
+            ld.qis[1].style.transform = "translateY(calc(-3rem * " + (pos.s) + "))";
+            ld.qis[2].style.transform = "translateY(calc(-2.5rem * " + (pos.s) + "))";
+            ld.qis[3].style.transform = "translateY(calc(-2rem * " + (pos.s) + "))";
+            ld.qis[4].style.transform = "translateY(calc(-1.5rem * " + (pos.s) + "))";
+            ld.qis[5].style.transform = "translateY(calc(-1rem * " + (pos.s) + "))";
+            ld.qis[6].style.transform = "translateY(calc(-0.5rem * " + (pos.s) + "))";
+        } 
+    } else {
+        if (!checkScrollDir(pos.a)) { // scrolling up
+            var _L = ld.qis.length - 1;
+            for (j = 0; j <= _L; j++) {
+                ld.qis[j].style.transform = "translateY(0px)";
+            }
+        } 
+    }
+
     // prefooter_sc
 
     pf.w1.style.transform = "translateY(" + (pos.y * -1) + "px)"; // 'wow' header, w
     pf.w2.style.transform = "translateY(" + (pos.y * 0.15) + "px)"; // o
     pf.w3.style.transform = "translateY(" + (pos.y * -0.5) + "px)"; // w
+
+    ///////////////////////////////////////////////////
 
     if (d > pos.st) { // check if scroll distance is valid (of a true scroll - prevents unwanted scrolling)
         if (getCookie("statsIncr") !== "true") { // check if cookie exists
@@ -144,203 +343,6 @@ function sc_L() { // functions (live on scroll)
         }*/
 
         //////////////////////////////////
-
-        im.L.style.transform = "translateY(" + (pos.y * (-1 * im.sM)) + "px)"; // #lead_sc
-        im.b5.style.transform = "translateY(" + (pos.y * -0.75) + "px)"; // bg-circle 5
-
-        if (b.Lp.top < 0 && !im.s7) { // #lead_point
-            im.s7 = true;
-            e_Fd(im.Lp1, true); // hide arrow 1
-        } else if (b.Lp.top > 0 && im.s7) {
-            im.s7 = false;
-            e_Fd(im.Lp1, false); // show arrow 1
-        }
-
-        if (b.Lps.top < 0 && !im.s8) { // #lead_point_2
-            im.s8 = true;
-            e_Fd(im.Lp2, true); // hide arrow 2
-        } else if (b.Lps.top > 0 && im.s8) {
-            im.s8 = false;
-            e_Fd(im.Lp2, false); // show arrow 2
-        }
-
-        if ((pos.r && !im.s5) || (!pos.r && im.s4) || (pos.r && im.s5)) { // downward scroll (plus, msc. conditions)
-
-            if (!im.s5 || !pos.aTs) {
-
-                if (b.e.top < aH) {
-
-                    im.Lp1.style.transform = "translateY(" + (pos.y * 0.45) + "px) rotate(90deg)"; // parallax arrow 1
-                    im.Lp2.style.transform = "translateY(" + (pos.y * 0.8) + "px) rotate(90deg)"; // parallax arrow 2
-                    im.s6 = false;
-                }
-
-                if (b.L.bottom < aH) {
-                    var r = aH - b.L.bottom;
-                    im.Lp3.style.transform = "translateX(" + ((r / 2) * 0.5) + "px) translateY(" + (r * -0.4) + "px)"; // parallax arrow 3
-                    im.Lp4.style.transform = "translateY(" + (r * -0.4) + "px)"; // parallax arrow 4 (hidden)
-                } else {
-                    im.Lp3.style.transform = "translateX(-2rem)"; // parallax arrow 3
-                }
-            } else {
-                if (pos.r && !im.s4) {
-                    im.p = op.d.getTime(); // get time of change
-                    im.s4 = true;
-                    setTimeout(function() {
-                        im.s4 = false;
-                    }, op.t);
-                } else if (pos.r && im.s4) {
-                    if ((op.d.getTime() - im.p) > op.t) {
-                        im.s5 = false; // confirmed
-                        im.s4 = false;
-                    }
-                }
-            }
-        }  
-        
-        if ((!pos.r || (pos.r && im.s4)) && !pos.aT) { // upward scroll
-
-            if (im.s5 && !pos.aTs) { // /*(!pos.aTs || !pos.aT */ ) {
-                if (b.e.top < aH) {
-                    im.Lp1.style.transform = "translateY(" + (pos.y * 0.45) + "px) rotate(-90deg)"; // parallax arrow 1
-                    im.Lp2.style.transform = "translateY(" + (pos.y * 0.8) + "px) rotate(-90deg)"; // parallax arrow 2
-                    im.s6 = true; 
-                }
-
-                if (b.L.bottom < aH) {
-                    var r = aH - b.L.bottom;
-                    im.Lp3.style.transform = "translateX(" + ((r / 2) * 0.5) + "px) translateY(" + (r * -0.4) + "px) rotate(180deg)"; // parallax arrow 3
-                    im.Lp4.style.transform = "translateY(" + (r * -0.4) + "px)"; // parallax arrow 4 (hidden)
-                } else {
-                    im.Lp3.style.transform = "translateX(-2rem) rotate(180deg)"; // parallax arrow 3
-                }
-            } else {
-                if (!pos.r && !im.s4) {
-                    im.p = op.d.getTime(); // get time of change
-                    im.s4 = true;
-                    setTimeout(function() {
-                        im.s4 = false;
-                    }, op.t);
-                } else if (!pos.r && im.s4) {
-                    if ((op.d.getTime() - im.p) > op.t) {
-                        im.s5 = true; // confirmed
-                        im.s4 = false;
-                    }
-                }
-            }
-        } else if (pos.aT && im.s5) {
-            im.s5 = false;
-            im.s4 = false;
-        }
-
-        if (b.pL3.bottom < (0 - b.pL3.height)) { // parallax arrow 3
-            im.s2 = true;
-            c_rep(im.Lp3, "d_i", "d_n");
-        } else if (b.pL4.bottom > (0 - b.pL4.height) && im.s2) {
-            im.s2 = false;
-            c_rep(im.Lp3, "d_n", "d_i");
-        }
-
-        if (b.Lpf.top <= ld.rf) { // c_Info header
-            ld.x2 = true;
-            if (!ld.x) {
-                ld.x = true;
-                ld.rfd = pos.y;
-            }
-            var m = (pos.y - ld.rfd) * (im.sM / (ld.rf - (ld.rfe * (1 - im.sM)))), // modifier
-                t = (-1 * (((pos.y * (1 + m)) - ld.rfd) * ((op.fN * 3.5) / (ld.rf - ld.rfe)))),
-                p = 1 - ((t) / (op.fN * -3.5));
-            if (t >= op.fN * -3.5) {
-                im.Lpf.style.transform = "translateY(" + t + "px)";
-                im.Lph.style.opacity = p;
-            } else if (t < op.fN * -3.5) {
-                im.Lpf.style.transform = "translateY(" + (op.fN * -3.5) + "px)";
-                im.Lph.style.opacity = "0";
-            }
-        } else if (ld.x2) {
-            ld.x2 = false;
-            im.Lpf.style.transform = "translateY(0px)";
-            im.Lph.style.opacity = "1";
-        }
-
-        ld.cs[0].style.transform = "translateY(" + (pos.y * -0.1) + "px)"; // c_Info 1
-        ld.cs[1].style.transform = "translateY(" + (pos.y * -0.15) + "px)"; // c_Info 2
-        ld.cs[2].style.transform = "translateY(" + (pos.y * -0.2) + "px)"; // c_Info 3
-        ld.cs[3].style.transform = "translateY(" + (pos.y * -0.25) + "px)"; // c_Info 4
-        ld.cs[4].style.transform = "translateY(" + (pos.y * -0.3) + "px)"; // c_Info 5
-
-        for (j = 0; j <= ld.cs.length - 1; j++) { // c_Info h5
-            if (ld.cs[j - 1]) {
-                if (b["csI" + (j + 1)].top < b["csI" + (j)].bottom) { // if 2 h5 elements intersect
-                    ld.x3 = true;
-                    break;
-                } else {
-                    ld.x3 = false;
-                }
-            }
-        }
-
-        if (ld.x3) { // hide c_Info
-            ld.csm.classList.add("o-img");
-
-            ld.csa[0].setAttribute("onclick", "event.preventDefault()"); // 'photos'
-            ld.csa[0].removeAttribute("href");
-            ld.csa[0].classList.add("u-d");
-
-            ld.csa[1].setAttribute("onclick", "event.preventDefault()"); // 'projects'
-            ld.csa[1].removeAttribute("href");
-            ld.csa[1].classList.add("u-d");
-        } else { // show
-            ld.csm.classList.remove("o-img");
-
-            ld.csa[0].setAttribute("onclick", "location.href='https://ivansojivarghese.github.io/clicks'");
-            ld.csa[0].setAttribute("href", "javascript:void(0)");
-            ld.csa[0].classList.remove("u-d");
-
-            ld.csa[1].setAttribute("onclick", "location.href='https://ivansojivarghese.github.io/code'");
-            ld.csa[1].setAttribute("href", "javascript:void(0)");
-            ld.csa[1].classList.remove("u-d");
-        }
-
-        // q_Info
-
-        if (b.pb.bottom > aH && b.Lps.top > aH) {
-            if (checkScrollDir(pos.a)) { // scrolling down
-                ld.qis[1].style.transform = "translateY(calc(0.5rem * " + (pos.s) + "))";
-                ld.qis[2].style.transform = "translateY(calc(1rem * " + (pos.s) + "))";
-                ld.qis[3].style.transform = "translateY(calc(1.5rem * " + (pos.s) + "))";
-                ld.qis[4].style.transform = "translateY(calc(2rem * " + (pos.s) + "))";
-                ld.qis[5].style.transform = "translateY(calc(2.5rem * " + (pos.s) + "))";
-                ld.qis[6].style.transform = "translateY(calc(3rem * " + (pos.s) + "))";
-                ld.qis[7].style.transform = "translateY(calc(3.5rem * " + (pos.s) + "))";
-            } 
-        } else {
-            if (checkScrollDir(pos.a)) { // scrolling down
-                var _L = ld.qis.length - 1;
-                for (j = 0; j <= _L; j++) {
-                    ld.qis[j].style.transform = "translateY(0px)";
-                }
-            } 
-        }
-
-        if (b.pb.bottom < 0 && b.Lps.top < 0) {
-            if (!checkScrollDir(pos.a)) { // scrolling up
-                ld.qis[0].style.transform = "translateY(calc(-3.5rem * " + (pos.s) + "))";
-                ld.qis[1].style.transform = "translateY(calc(-3rem * " + (pos.s) + "))";
-                ld.qis[2].style.transform = "translateY(calc(-2.5rem * " + (pos.s) + "))";
-                ld.qis[3].style.transform = "translateY(calc(-2rem * " + (pos.s) + "))";
-                ld.qis[4].style.transform = "translateY(calc(-1.5rem * " + (pos.s) + "))";
-                ld.qis[5].style.transform = "translateY(calc(-1rem * " + (pos.s) + "))";
-                ld.qis[6].style.transform = "translateY(calc(-0.5rem * " + (pos.s) + "))";
-            } 
-        } else {
-            if (!checkScrollDir(pos.a)) { // scrolling up
-                var _L = ld.qis.length - 1;
-                for (j = 0; j <= _L; j++) {
-                    ld.qis[j].style.transform = "translateY(0px)";
-                }
-            } 
-        }
 
     } else {
         if (!pos.s) { // if no scrolling
