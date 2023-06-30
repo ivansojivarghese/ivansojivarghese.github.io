986a4e1449074f392706d5151b42abd99a41ebd4
ivansojivarghese <ivansojivarghese@gmail.com>
2023-06-30 11:20:49 +0800

    Update scr.js

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -294,8 +294,8 @@ function sc_L() { // functions (live on scroll)
         }
 
         pf.w1.style.transform = "translateY(" + (pos.y * -0.3) + "px)"; // 'wow' header, w
-        pf.w2.style.transform = "translateY(" + (pos.y * -0.1) + "px)"; // o
-        pf.w3.style.transform = "translateY(" + (pos.y * -0.2) + "px)"; // w
+        pf.w2.style.transform = "translateY(" + (pos.y * -0.075) + "px)"; // o
+        pf.w3.style.transform = "translateY(" + (pos.y * -0.3) + "px)"; // w
     }
 
     requestAnimationFrame(sc_L);
