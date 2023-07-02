a7fcb758d83fbd13ea299471fb17a3ff26a79265
ivansojivarghese <ivansojivarghese@gmail.com>
2023-07-02 12:26:21 +0800

    Update scr.js

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -274,7 +274,7 @@ function sc_L() { // functions (live on scroll)
 
         // pf.w2.style.transform = "translateX(-0.9rem) translateY(" + (pos.y * 0.2175) + "px)"; // o
 
-    } else if (pf.x && b.pfw3.bottom < b.pfw2.top) {
+    } else if (pf.x) {
 
         var t = ((0.2175 - 0.15) / (op.fN * 3.5)) * (pos.y - pf.r);
         if (t <= 0.2175 && t > 0) {
