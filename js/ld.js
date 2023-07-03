93dc784419fd5a92e21be69b90a385f330f02b7f
ivansojivarghese <ivansojivarghese@gmail.com>
2023-07-03 15:38:20 +0800

    Update ld.js

--- js/ld.js
+++ js/ld.js
@@ -937,7 +937,7 @@ function load_jscss_N() { // load up JS/CSS (after page load; common)
 
     c_css("#footer_sc .w-s", "height: calc(" + h + "px - 6rem);", false, null); // set height of footer design element
     if (document.getElementById("pf_scrollbar")) {
-        c_css("#pf_scrollbar", "bottom: calc(" + h + "px)", false, null);
+        c_css("#pf_scrollbar", "bottom: calc(" + hm + "px)", false, null);
     }
     fter.y.innerHTML = y;
     while (fter.v[i]) { // add copyright year + site version no.
