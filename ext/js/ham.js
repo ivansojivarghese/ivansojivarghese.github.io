d3db2d91dc181e6e36a8ce30875c1dc6ad6c012b
ivansojivarghese <ivansojivarghese@gmail.com>
2023-07-05 18:16:28 +0800

    Update ham.js

--- ext/js/ham.js
+++ ext/js/ham.js
@@ -46,6 +46,7 @@ function hm_L() {
             var j = 0,
                 v = [1, 1.75, 2.5],
                 showStroke = function() {
+                    /*
                     if (j < hm.k.length) {
                         e_Fd(hm.k[j], false);
                         hm.k[j].classList.add("z_F");
@@ -55,6 +56,59 @@ function hm_L() {
                         hm.b.addEventListener("click", h_mTg); // hamburger menu toggle (open/close)
                         hm.bL = true;
                         clearInterval(hm.bLS);
+                    }*/
+                    if (!hm.bL && pos.aT) {
+                        if (j < hm.k.length || hamCheckStrokes(hm.k, true).s) {
+    
+                            if (j < hm.k.length) {
+                                e_Fd(hm.k[j], false);
+                                hm.k[j].classList.add("z_F");
+                                hm.k[j].style.width = v[j] + "rem";
+                                j++;
+                            } else if (hamCheckStrokes(hm.k, true).s) {
+                                var obj = hamCheckStrokes(hm.k, true),
+                                    d = obj.i[0];
+                                
+                                e_Fd(hm.k[d], false);
+                                hm.k[d].classList.add("z_F");
+                                hm.k[d].style.width = v[d] + "rem";
+    
+                                // console.log("d:" + d); 
+                            }
+    
+                        } else {
+                            hm.b.addEventListener("click", h_mTg); // hamburger menu toggle (open/close)
+                            hm.bL = true;
+                            clearInterval(hm.bLS);
+                        }
+                    } else {
+    
+                        // hide visible strokes
+                        var i = 0,
+                            hideStroke = function() {
+                                if (!pos.aT) {
+                                    if (i >= 0) {
+    
+                                        e_Fd(hm.k[i], true);
+                                        hm.k[i].classList.remove("z_F");
+                                        hm.k[i].style.width = "";
+                                        i--;
+              
+                                        // console.log("i:" + i);
+    
+                                    } else {
+                                        hm.b.removeEventListener("click", h_mTg); // hamburger menu toggle (open/close)
+                                        hm.bL = false;
+                                        j = hm.k.length - 1;
+                                        clearInterval(hm.bLHe);
+                                    }
+                                }
+                            };
+                        if (!hm.x2) {
+                            hm.x2 = true;
+                            hideStroke();
+                            hm.bLHe = setInterval(hideStroke, (op.t / 2));
+                        }
                     }
                 };
 
