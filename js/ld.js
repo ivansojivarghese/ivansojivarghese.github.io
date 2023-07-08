03e351fe6878e93507b608bf681848dc490c430f
ivansojivarghese <ivansojivarghese@gmail.com>
2023-07-08 11:42:43 +0800

    update

--- js/ld.js
+++ js/ld.js
@@ -1306,5 +1306,9 @@ load_css();
 rL.p.addEventListener("animationiteration", load_e); // read a function upon every loading ring iteration (transversing)
 history.scrollRestoration = "manual"; // prevent automatic scroll rendering from browser (in memory)
 setTimeout(function() {
+    if (UMB && !op.bNx) { // check if current platform is up-to-date
+        op.bNx = true;
+        browserCheck(true); // secondary check if online API is available
+    }
     _Ld = setInterval(docRead, op.Ls); // run 'load' scripts upon startup
 }, op.t);
