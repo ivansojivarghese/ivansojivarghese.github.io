c3074b8b8d27e8eee313bd5729aaeac8faa19cc6
ivansojivarghese <ivansojivarghese@gmail.com>
2023-07-24 14:19:35 +0800

    Update scr.js

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -323,7 +323,7 @@ function sc_L() { // functions (live on scroll)
 
     // scroll
 
-    if (b.pfsc.bottom && ((b.pfsc.bottom / 2) < aH) && !pf.x) {
+    if (b.pfsc.top && (b.pfsc.top < aH) && !pf.x) {
         pf.x = true;
         pf.sc.classList.add("inview");
     }
