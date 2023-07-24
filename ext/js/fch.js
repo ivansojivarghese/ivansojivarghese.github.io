dd237a49da7015170d10a41cb6e6bd87d04e8ccf
ivansojivarghese <ivansojivarghese@gmail.com>
2023-07-24 20:28:57 +0800

    Update fch.js

--- ext/js/fch.js
+++ ext/js/fch.js
@@ -179,7 +179,7 @@ function load_css_e() { // load CSS styles (page specific)
             timeNow(el.Ltd); // show time
             el.Lt.classList.remove("d_n");
 
-            userCity = ipAPI("city").json(); // get user's nearest city (API)
+            userCity = ipAPI("city"); // get user's nearest city (API)
         }
     }
 
