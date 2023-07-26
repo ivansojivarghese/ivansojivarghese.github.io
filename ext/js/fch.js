ec3d1a05de152476f24f56a86dbcdf86cd4f7d46
ivansojivarghese <ivansojivarghese@gmail.com>
2023-07-26 13:48:42 +0800

    update

--- ext/js/fch.js
+++ ext/js/fch.js
@@ -304,13 +304,18 @@ function load_js_e() { // load JS (page specific)
                         // EDIT INFO TO HTML
                         weather.r.innerHTML = Math.round(weatherAPIres.main.temp); // weather reading
                         weather.u.innerHTML = (tempUnit(ipAPIres.country) === "metric") ? "C" : "F"; // weather unit
-                        fetch("https://openweathermap.org/img/wn/" + weatherAPIres.weather["0"].icon + "@2x.png") // weather icon
-                            .then((response) => {
-                                weather.i.style.backgroundImage = "url('https://openweathermap.org/img/wn/" + weatherAPIres.weather["0"].icon + "@2x.png')";
-                                apiSuccess = true;
-                            }).catch((error) => {
-                                apiSuccess = null;
-                            });
+
+                        var weatherIcon = async function() {
+                            await fetch("https://openweathermap.org/img/wn/" + weatherAPIres.weather["0"].icon + "@2x.png") // weather icon
+                                .then((response) => {
+                                    weather.i.style.backgroundImage = "url('https://openweathermap.org/img/wn/" + weatherAPIres.weather["0"].icon + "@2x.png')";
+                                    apiSuccess = true;
+                                }).catch((error) => {
+                                    apiSuccess = null;
+                                });
+                        }
+
+                        weatherIcon();
 
                         // weather.i.style.backgroundImage = "url('https://openweathermap.org/img/wn/" + weatherAPIres.weather["0"].icon + "@2x.png')";
 
