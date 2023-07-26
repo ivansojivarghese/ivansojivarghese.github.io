bb4696f4299d9d1f70a5a6d77118f3554a0e1aac
ivansojivarghese <ivansojivarghese@gmail.com>
2023-07-26 21:56:20 +0800

    Update fch.js

--- ext/js/fch.js
+++ ext/js/fch.js
@@ -57,6 +57,7 @@ var fchL = {
         lk3 : document.getElementById("link_3"),
         lk3a : document.getElementById("link_3a"),
         lk3b : document.getElementById("link_3b"),
+        lk4 : document.getElementById("link_4"),
         chev : document.getElementById("chev_d"),
         bgC : document.querySelectorAll(".bg-circle"),
         bgC4 : document.getElementById("bg-cir4"),
@@ -452,7 +453,7 @@ function load_eN() { // load, after cookie acceptance (page specific)
         }
     } else if (vw.dk) { // desktop
 
-        
+        el.lk4.classList.remove("z_wd"); // add width on link_4
 
     }
 }
