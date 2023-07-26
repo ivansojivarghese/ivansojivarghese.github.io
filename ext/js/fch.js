a92fb8628c2f3f1b45383dd9fd443f63ab8b2f6b
ivansojivarghese <ivansojivarghese@gmail.com>
2023-07-27 02:08:50 +0800

    update

--- ext/js/fch.js
+++ ext/js/fch.js
@@ -471,6 +471,8 @@ function load_eN() { // load, after cookie acceptance (page specific)
             };
         el.lk4.classList.remove("z_wd"); // add width on link_4
         s(); // start navbar animations
+        scrollArrowIterate(true, el.lk3c, "0", "2.5rem", "")
+
         el.lk3c.style.height = "2.5rem";
     }
 }
