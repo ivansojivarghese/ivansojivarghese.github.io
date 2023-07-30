c7f179613495e2b13b581f0c60f76bba27a15441
ivansojivarghese <ivansojivarghese@gmail.com>
2023-07-30 17:34:36 +0800

    Update ef.js

--- js/ef.js
+++ js/ef.js
@@ -206,3 +206,33 @@ function resetState() {
 function updateText(text, el) {
     el.innerHTML = text;
 }
+
+// JavaScript program to calculate Distance Between
+// Two Points on Earth
+ 
+function coordsDistance (lat1, lat2, lon1, lon2) { // REFERENCED FROM @https://www.geeksforgeeks.org/program-distance-two-points-earth/
+
+    // The math module contains a function
+    // named toRadians which converts from
+    // degrees to radians.
+    lon1 =  lon1 * Math.PI / 180;
+    lon2 = lon2 * Math.PI / 180;
+    lat1 = lat1 * Math.PI / 180;
+    lat2 = lat2 * Math.PI / 180;
+
+    // Haversine formula
+    let dlon = lon2 - lon1;
+    let dlat = lat2 - lat1;
+    let a = Math.pow(Math.sin(dlat / 2), 2)
+    + Math.cos(lat1) * Math.cos(lat2)
+    * Math.pow(Math.sin(dlon / 2),2);
+
+    let c = 2 * Math.asin(Math.sqrt(a));
+
+    // Radius of earth in kilometers. Use 3956
+    // for miles
+    let r = 6371;
+
+    // calculate the result
+    return(c * r);
+}
