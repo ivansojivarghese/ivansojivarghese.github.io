f098dec6d8e56fab43d9b59fbec4b27931452af0
ivansojivarghese <ivansojivarghese@gmail.com>
2023-07-31 17:53:04 +0800

    Update fch.js

--- ext/js/fch.js
+++ ext/js/fch.js
@@ -338,7 +338,7 @@ function load_js_e() { // load JS (page specific)
         var dkAPI = function() {
             if (ipAPIres.error) {
 
-                apiSuccess = null;
+                apiSuccess = null; //
 
                 ///////
                 // SET a loop to continously check for variable within timeout time
