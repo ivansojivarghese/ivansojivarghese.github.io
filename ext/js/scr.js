32b11aafd9bd8f4f3f104bcab5358dfbc9ecced7
ivansojivarghese <ivansojivarghese@gmail.com>
2023-08-02 20:26:06 +0800

    Update scr.js

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -694,6 +694,7 @@ function sc_LpH() { // scroll loop - phablet
                                 el.wCh[a][wInfo_f[a] - 1].style.width = nwd + "px";
                             }
                         } else if (target_bd.right < wiD && target_bd.left > 0) { // CENTRE (not infringing of EDGES)
+                            /*
                             var iwd = getCSSProperty(targets[c], "width"),
                                 wd = wiD - target_bd.right,
                                 nwd = iwd + wd; // new (total) width
@@ -701,7 +702,7 @@ function sc_LpH() { // scroll loop - phablet
                                 el.wCh[a][wInfo_f[a] - 1].style.width = nwd + "px"; //
                             } else if (!targets[c].classList.contains("actv")) {
                                 el.wCh[a][wInfo_f[a] - 1].classList.add("actv"); // activated (fully shown on screen)
-                            }
+                            }*/
                         }
 
                         // new words (using comparison of live-width additions with viewport width)
