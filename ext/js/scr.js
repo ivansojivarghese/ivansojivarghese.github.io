93103664d7f8f38606f645d3aebf470ca4dc4e51
ivansojivarghese <ivansojivarghese@gmail.com>
2023-08-03 17:14:46 +0800

    update

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -770,7 +770,7 @@ function sc_LpH() { // scroll loop - phablet
 
                             targets[c].remove(); // REMOVE from DOM
 
-                            el.wCh[a].appendChild(wC_clone_hold[0]); // append to last of line
+                            el.wChe[a].appendChild(wC_clone_hold[0]); // append to last of line
                         }
 
                         // new words (using comparison of live-width additions with viewport width) //
