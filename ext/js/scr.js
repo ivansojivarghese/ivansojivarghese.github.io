c34a5c3a9116aada8a259d414fb0e2469edbdaa7
ivansojivarghese <ivansojivarghese@gmail.com>
2023-08-03 15:04:48 +0800

    Update scr.js

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -779,7 +779,7 @@ function sc_LpH() { // scroll loop - phablet
 
                                 if (el.wCh[a][wInfo_f[a]].classList.contains("z_O")) {
 
-                                    console.log("fa: " + wInfo_f[a] + ", gi " + getIndex(targets[c]));
+                                    console.log("fa: " + wInfo_f[a] + ", gi: " + getIndex(targets[c]));
 
                                     // wC_hold[wC_hold.length] = getIndex(targets[c]); // add to hold
 
