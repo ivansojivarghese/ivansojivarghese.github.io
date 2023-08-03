05b249de95d91f57cd15035210ace8ac8eae7b39
ivansojivarghese <ivansojivarghese@gmail.com>
2023-08-03 10:42:16 +0800

    Update scr.js

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -697,7 +697,14 @@ function sc_LpH() { // scroll loop - phablet
                                 targets[c].style.width = (wiD - target_bd.left) + "px";
                             }*/
 
-                        } /*else if (target_bd.right >= wiD) { // RIGHT
+                        } else if (target_bd.right < 0) { // RIGHT ""
+
+                            targets[c].classList.remove("v_s");
+                            targets[c].classList.add("v_n");
+
+                        }
+                        
+                        /*else if (target_bd.right >= wiD) { // RIGHT
 
                             // var wd = wiD - totalWidth(targets);
                             var iwd = getCSSProperty(targets[c], "width"),
