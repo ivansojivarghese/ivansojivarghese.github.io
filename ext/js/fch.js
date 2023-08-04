80464285d11b67119cd757c6c041a284fef317d0
ivansojivarghese <ivansojivarghese@gmail.com>
2023-08-04 21:52:12 +0800

    Update fch.js

--- ext/js/fch.js
+++ ext/js/fch.js
@@ -624,6 +624,8 @@ function load_eN() { // load, after cookie acceptance (page specific)
                     wInfo_s[a] += wd; // update total line width
                     wInfo_i[a][b] = wd; // update indiv. word widths
 
+                    el.wCh[a][b].classList.add("p-a");
+
                     if (wInfo_s[a] > wiD) { // check if within viewport width (single-line) space
 
                         // el.wCh[a][b].classList.add("d_n", "z_O", "trs"); // else, hide
