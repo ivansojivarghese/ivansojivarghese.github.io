33b7101e97d0643c6fe4da6a14141589d5542304
ivansojivarghese <ivansojivarghese@gmail.com>
2023-08-05 15:45:23 +0800

    update

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -591,7 +591,7 @@ function sc_LpH() { // scroll loop - phablet
             };
 
         if (!pos.aT) { // during scroll
-            if (b.wd.bottom < (aH - (op.fN + (op.fN * 0.8)))) { 
+            if (b.wd.bottom < (aH - (op.fN + (op.fN * 0.8)))) { // transform till bottom threshold
                 im.wd.style.transform = "translateY(" + (pos.y * 0.05) + "px)";
             }
             im.gh.style.transform = "translateY(" + (ghTransform) + "px)";
