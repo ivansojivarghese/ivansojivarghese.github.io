9159ab67f421a4b33f0d2b9cd5e79399776d4bf1
ivansojivarghese <ivansojivarghese@gmail.com>
2023-08-09 11:45:47 +0800

    update

--- js/msc.js
+++ js/msc.js
@@ -3023,6 +3023,7 @@ screen.orientation.addEventListener("change", function() { // mobile/tablet orie
 
                         vw.mB_L = false;
                         // eR.s = false;
+                        
                         // STATIC ERRORS (UPDATE WHEN NEEDED!)
                         if ((op.b.f || op.sys === null || op.bN || op.pl) && (devicePerformance(op.pSpd, op.sfr, op.pCores) > 0 && rL.i)) { // check for platform support
                             if (op.b.f) {
