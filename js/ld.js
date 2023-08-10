d5b3f5cb1fd7844082e37d71330b9d965ea65d8a
ivansojivarghese <ivansojivarghese@gmail.com>
2023-08-10 19:41:32 +0800

    Update ld.js

--- js/ld.js
+++ js/ld.js
@@ -1422,8 +1422,7 @@ function engLangUpdate(v) { // update eng. language variant
                     for (var h in engAPIres) { // loop through en-GB to en-US dictionary object properties
                         var word = h.toString();
                         if (newArr[j].toLowerCase() === word.toLowerCase()) { // find a matching word
-
-                            // console.log(newArr[j] + ", " + wordType(newArr[j])); 
+                            var wdType = wordType(newArr[j]); // determine type of word scanned
 
                             console.log(newArr[j]);
 
