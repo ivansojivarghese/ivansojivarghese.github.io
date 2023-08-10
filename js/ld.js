e5c21c47620eddacde52fb859cdd71273a5338dc
ivansojivarghese <ivansojivarghese@gmail.com>
2023-08-10 09:52:21 +0800

    Update ld.js

--- js/ld.js
+++ js/ld.js
@@ -824,7 +824,7 @@ function load_e() { // end the loading sequence
                         if (js_load_e() || op.er.ft) { // if a fatal error
                             eR.h = "ft";
                             eR.p = "ft";
-                        } else if ((ipAPIres.online && (tz !== ipAPIres.timezone)) || (ipifyAPIres.online && timeAPIres.online && (tz !== timeAPIres.timezone)) || (clientAPIres.online && clientAPIres.isBehindProxy) || (roamingAPIres.online && roamingAPIres.isRoaming)) {
+                        } else if ((ipAPIres.online && (op.tz !== ipAPIres.timezone)) || (ipifyAPIres.online && timeAPIres.online && (op.tz !== timeAPIres.timezone)) || (clientAPIres.online && clientAPIres.isBehindProxy) || (roamingAPIres.online && roamingAPIres.isRoaming)) {
                             
                             var address = "";
                             eR.h = "vp";
