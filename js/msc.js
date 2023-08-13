0f797ccbc97149df48cde876f70c7114851bd522
ivansojivarghese <ivansojivarghese@gmail.com>
2023-08-13 16:32:59 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -3431,9 +3431,11 @@ window.addEventListener("blur", function() { // window out of focus
 
 window.addEventListener("beforeunload", function(e) { // DUPLICATE TAB detection (LOCAL browsers only)
     var windowCount = window.localStorage.getItem('num_windows');  
-    if (windowCount != null || windowCount !== 1) {
+    if (windowCount != null) {
         windowCount = parseInt(windowCount) - 1;
         window.localStorage.setItem('num_windows', windowCount);
+    } else if (windowCount == 0) {
+        window.localStorage.setItem('num_windows', null);
     }
  }, false);
  
