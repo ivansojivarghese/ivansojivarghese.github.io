1c0734b7069879aa3a2d786e942ce12dc4d03ef0
ivansojivarghese <ivansojivarghese@gmail.com>
2023-08-25 23:33:49 +0800

    Update fch.js

--- ext/js/fch.js
+++ ext/js/fch.js
@@ -771,10 +771,9 @@ function peekDesktop() { // 'scroll' letter transform effect
     if ((m + j) < c) {
         el.lk3cbs[m + j].classList.remove("z_O");
         el.lk3cbs[m + j].classList.add("up", "ex", "z_F");
-    }
-
-    if (j < (m - 1)) {
-        setTimeout(peekDesktop(), (op.t / 2));
+        if (j < (m - 1)) {
+            setTimeout(peekDesktop(), (op.t / 2));
+        }
     }
 
     /*
