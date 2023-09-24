e0c65cd9056bc8093bd9fc698ce50a78604ba3df
ivansojivarghese <ivansojivarghese@gmail.com>
2023-09-24 17:16:06 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -511,7 +511,7 @@ op = {
     Ls : op.Ls,
     pSpda : [], 
     pSpd : 0, // device processor speed (average estimated in GHZ)
-    pMin : [6, 15], // min./rec. processor speed
+    pMin : [8, 15], // min./rec. processor speed
     pCores : navigator.hardwareConcurrency, // no. of cpu logical cores
     pCoresMin : [2, 6], // cores min./rec.
     sfra : [],
