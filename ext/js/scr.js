02d322b5cb0c9683aa9cd0d504a98d4d58ee6c72
ivansojivarghese <ivansojivarghese@gmail.com>
2023-10-03 19:28:57 +0800

    Update scr.js

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -790,7 +790,7 @@ function sc_LpH() { // scroll loop - phablet
                     } else {
 
                         var yBounce = (pf.rf12 - ((pos.y - pf.rf14) / op.fN)),
-                            xBounce = ((pf.rf11 - rbLeft) / op.fN) - (Math.cos(degToRad(10)) * (pf.rf12 - yBounce));
+                            xBounce = ((pf.rf8 - rbLeft) / op.fN) - (Math.cos(degToRad(5)) * (pf.rf12 - yBounce));
 
                         pf.rbc[0].style.transform = "translateX(" + xBounce + "rem) translateY(" + yBounce + "rem)";
                         pf.rbc[1].style.left = ((((pf.rf10 + pf.rf9) - (pos.y * 0.975)) / pf.rf9) * 100) + "%";
