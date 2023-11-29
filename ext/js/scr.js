9794388eddd5a2fc913229c4788273fa8cd9a358
ivansojivarghese <ivansojivarghese@gmail.com>
2023-11-29 17:25:46 +0800

    Update scr.js

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -1000,7 +1000,7 @@ function sc_LpH() { // scroll loop - phablet
 
                         xLoop = setInterval(function() {
                             liveX += 60; // default fps
-                            var u = (liveX <= (wiD / (20 / 11))) ? (((liveX / (wiD / 2)) * (fnVel - inVel)) + inVel) : ((((liveX - (wiD / 2)) / (wiD / 2)) * (fnVel - inVel)) + inVel);
+                            var u = (liveX <= (wiD / (20 / 11))) ? (((liveX / (wiD / 2)) * (fnVel - inVel)) + inVel) : (fnVel - (((liveX - (wiD / 2)) / (wiD / 2)) * (fnVel - inVel)));
                             pf.ld_p.style.transitionDuration = u + "s";  // slower duration as flight progresses up
 
                             console.log(u);
