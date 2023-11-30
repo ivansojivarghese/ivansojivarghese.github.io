c43cef9d7dcebdfa6f3476324ccb3331ec5d599a
ivansojivarghese <ivansojivarghese@gmail.com>
2023-11-30 11:57:41 +0800

    Update scr.js

--- ext/js/scr.js
+++ ext/js/scr.js
@@ -1007,7 +1007,7 @@ function sc_LpH() { // scroll loop - phablet
                                 xDivideMod = 1;
                             }
 
-                            liveX += dev.t; // default fps increment
+                            liveX += (dev.t / 2); // default fps increment
                             conAngle = angleMod * (inAngle + ((90 - (Math.atan(model(liveX) / ((wiD / xDivideMod) - liveX)) * (180 / Math.PI))))); // initial angle
 
                             console.log((model(wiD / xDivide) - model(liveX)) + ", " + ((wiD / xDivide) - liveX));
