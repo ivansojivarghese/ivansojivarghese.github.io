c8a7b06ba8b45ad6c7b19f75c8fc84489fa29459
ivansojivarghese <ivansojivarghese@gmail.com>
2023-12-16 16:02:43 +0800

    Update fch.js

--- ext/js/fch.js
+++ ext/js/fch.js
@@ -207,7 +207,7 @@ for (i = 0; i < dev.skillsNum; i++) {
 function toggleColorMode_e() { // toggle between light and dark modes (page specific)
     var dw_img = (!op.darkMode) ? document.querySelector(".download_img") : document.querySelector(".download_w_img"), // download 
         ch_img = (!op.darkMode) ? document.querySelector(".chevron_down_img") : document.querySelector(".chevron_down_w_img"), // chevron
-        logo_h_img = (!op.darkMode) ? document.querySelector(".logo_hybrid") : document.querySelector(".logo-hybrid-inverse"); // logo-hybrid
+        logo_h_img = (!op.darkMode) ? document.querySelector(".logo-hybrid") : document.querySelector(".logo-hybrid-inverse"); // logo-hybrid
 
     if (!op.darkMode) { // if light, change to dark
 
