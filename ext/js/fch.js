6eb4eb49cf497774b6bcbaa3308db7a02f1179b6
ivansojivarghese <ivansojivarghese@gmail.com>
2023-12-16 20:13:15 +0800

    Update fch.js

--- ext/js/fch.js
+++ ext/js/fch.js
@@ -211,7 +211,7 @@ for (i = 0; i < dev.skillsNum; i++) {
 var hamScZdx = function() {
         var h_sc = document.getElementById("ham_sc"),
             lk3 = document.getElementById("link_3");
-        if (hm.z) {
+        if (!hm.z) {
             lk3.classList.add("z-G");
         } else {
             lk3.classList.remove("z-G");
