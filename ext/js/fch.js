dc39e71f71529689cc311e3748ef3072c7619398
ivansojivarghese <ivansojivarghese@gmail.com>
2023-12-18 14:45:14 +0800

    Update fch.js

--- ext/js/fch.js
+++ ext/js/fch.js
@@ -304,7 +304,7 @@ function toggleColorMode_e() { // toggle between light and dark modes (page spec
             }
 
             c_css("div.cursorInt", "background-color: #3D3D3D;", false, null, op, "darkMode");
-            c_css("#localWeather .img_icon.d_i", "background-color: #969696", false, null, op, "darkMode");
+            c_css("#localWeather .img_icon.d_i", "background-color: #C8C6C1", false, null, op, "darkMode");
             c_css("#link_4, #wordsTyperDet, #wordsTyperDet_cursor", "opacity: 0.5;", false, null, op, "darkMode");
 
             lb_img.classList.remove("lightbulb_img");
