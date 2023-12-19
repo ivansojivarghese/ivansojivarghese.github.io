0714dac565eb9e0344aa0792100fe23f7c44eda8
ivansojivarghese <ivansojivarghese@gmail.com>
2023-12-19 09:55:52 +0800

    Update os.js

--- js/os.js
+++ js/os.js
@@ -24,8 +24,10 @@ function isDarkMode() { // dark mode detection
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change',({ matches }) => { // detect color theme (live) change
     if (matches) { // change to dark mode
         toggleColorMode(null);
+        op.darkMode = true;
     } else { // change to light mode
         toggleColorMode(null);
+        op.darkMode = false;
     }
 });
 
