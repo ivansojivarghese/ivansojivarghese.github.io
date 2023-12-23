c0fd992093e983b89d159a157e808ca06d34ae7f
ivansojivarghese <ivansojivarghese@gmail.com>
2023-12-23 14:34:30 +1000

    update

--- js/ld.js
+++ js/ld.js
@@ -1404,8 +1404,9 @@ function errorCheck() { // check for errors
     ipAPI(""); // get user IP information API (ENTER A region IP value for testing, "/" + IP Address)
     if (ipAPIres.online) {
         ipAPI2(ipAPIres.ip); // get IP information API 2
-
-        
+        if (ipAPIres.city !== ipAPI2res.city) {
+            
+        }
     }
     clientAPI(); // get user IP information + proxy usage status (no arguments)
     if (ipAPIres.online) { // get USER ROAMING information using coords
