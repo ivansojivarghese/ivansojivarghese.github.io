980c68262670e3fb7bcd19c0e2c2bf7ff77f4a13
ivansojivarghese <ivansojivarghese@gmail.com>
2023-12-27 10:33:29 +1000

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -287,9 +287,9 @@ async function ipAPI(v) {  // Free usage, unlimited, https://www.findip.net/
             return response.json().then((data) => {
                 ipAPIres = data;
 
-                ipAPIres.lat = ipAPI2res.location.latitude;
-                ipAPIres.lon = ipAPI2res.location.longitude;
-                ipAPIres.city = ipAPI2res.city.names.en;
+                ipAPIres.lat = ipAPIres.location.latitude;
+                ipAPIres.lon = ipAPIres.location.longitude;
+                ipAPIres.city = ipAPIres.city.names.en;
 
                 /*
                 ipAPIres.lat = data.loc.slice(0, ipAPIres.loc.indexOf(",")), // get user latitude;
@@ -311,8 +311,8 @@ async function ipAPI2(v) {  // 50,000 per month limit, https://ipinfo.io/
                 const data = await response.json();
                 ipAPI2res = data;
 
-                ipAPI2res.lat = data.loc.slice(0, ipAPIres.loc.indexOf(",")), // get user latitude;
-                ipAPI2res.lon = data.loc.slice(ipAPIres.loc.indexOf(",") + 1, ipAPIres.loc.length), // get user longitude;
+                ipAPI2res.lat = data.loc.slice(0, ipAPI2res.loc.indexOf(",")), // get user latitude;
+                ipAPI2res.lon = data.loc.slice(ipAPI2res.loc.indexOf(",") + 1, ipAPI2res.loc.length), // get user longitude;
 
                 /*
                 ipAPI2res.lat = ipAPI2res.location.latitude;
