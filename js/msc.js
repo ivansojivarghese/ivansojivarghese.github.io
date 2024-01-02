1a41158b00290b9e33793de96babd5ed97d6d34f
ivansojivarghese <ivansojivarghese@gmail.com>
2024-01-02 13:19:20 +1000

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -757,7 +757,7 @@ op = {
         p : null // info
     },
     col : { // colors (hex)
-        b : "#303030", // base
+        b : !op.darkMode ? "#303030" : "#FFF", // base
         p : "#007000", // predicate
         n : "#A10000" // negate
     },
