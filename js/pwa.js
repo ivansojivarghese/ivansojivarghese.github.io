ed2b7aec310fdc22f3d3502b028fb4c5153831f8
ivansojivarghese <ivansojivarghese@gmail.com>
2024-01-04 13:05:43 +0800

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -4,5 +4,6 @@ function navButtonActive(b, e) {
     if (!target.classList.contains("buttonActive")) {
         target.children[0].style.backgroundImage = "url('../pwa/" + b + "_active.png')";
         e_Fd(target.children[0].children[0], false); 
+        target.classList.add("buttonActive");
     }
 }
