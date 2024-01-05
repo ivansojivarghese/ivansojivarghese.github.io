df8995648d871e6403894b8624aef907c171af73
ivansojivarghese <ivansojivarghese@gmail.com>
2024-01-05 11:45:25 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -253,6 +253,7 @@ document.body.addEventListener("scroll", setActivityTime);
 document.body.addEventListener("touchstart", setActivityTime);
 const refresh = () => {
     if (new Date().getTime() - time >= (dev.v * 24 * 60 * 60 * 1000)) { // max-duration limit in ms. (from days)
+        console.log("reL");
         reL(); // reload
     } else {
         setTimeout(refresh, 10000); // Check every 10s
