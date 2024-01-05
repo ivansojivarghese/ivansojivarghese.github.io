58d08a0f10c1aae5a7e21965208b191278149451
ivansojivarghese <ivansojivarghese@gmail.com>
2024-01-05 10:39:50 +0800

    update

--- js/pwa.js
+++ js/pwa.js
@@ -23,7 +23,7 @@ function timeOfDay() {
 function fetchPWAInfo() {
     const temp = document.querySelector('.pwa .weather #temp');
     const unit = document.querySelector('.pwa .weather #unit');
-    const greeting = document.querySelector('.pwa .weather #greeting');
+    const greeting = document.querySelector('.pwa .banner #greeting');
 
     greeting.innerHTML = timeOfDay();
     temp.innerHTML = Math.round(weatherAPIres.main.temp);
