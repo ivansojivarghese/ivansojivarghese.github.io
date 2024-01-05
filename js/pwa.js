fc098ea0edd8a0eac32c16919afde3a484636eb3
ivansojivarghese <ivansojivarghese@gmail.com>
2024-01-05 10:34:13 +0800

    update

--- js/pwa.js
+++ js/pwa.js
@@ -8,10 +8,24 @@ function navButtonActive(b, e) {
     }
 }
 
+function timeOfDay() {
+    var d = new Date();
+    time = d.getHours();
+    if (time < 12) {
+        return "morning";
+    } else if (time < 17) {
+        return "afternoon";
+    } else if (time < 24) {
+        return "evening";
+    }
+}
+
 function fetchPWAInfo() {
     const temp = document.querySelector('.pwa .weather #temp');
     const unit = document.querySelector('.pwa .weather #unit');
+    const greeting = document.querySelector('.pwa .weather #greeting');
 
+    greeting.innerHTML = timeOfDay();
     temp.innerHTML = Math.round(weatherAPIres.main.temp);
     unit.innerHTML = (tempUnit(ipAPIres.country.iso_code) === "metric") ? "C" : "F";
 }
