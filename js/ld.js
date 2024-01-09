f221a1b6781d043277b2be94cda86060fc00cb4e
ivansojivarghese <ivansojivarghese@gmail.com>
2024-01-09 11:26:40 +0800

    update

--- js/ld.js
+++ js/ld.js
@@ -758,7 +758,7 @@ function docRead() {
                                             clearInterval(weather_L);
 
                                             const tempIcon = document.querySelector('.pwa .weatherIcon');
-                                            $(tempIcon).load("weather/partly_cloudy.html", function() {
+                                            $(tempIcon).load("weather/mostly_sunny.html", function() {
                                                 fetchPWAInfo();
                                                 setTimeout(function() {
 
