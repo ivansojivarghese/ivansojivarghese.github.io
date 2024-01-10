c7420ac72b121eb88691fd48b26fcdd30b9f0c80
ivansojivarghese <ivansojivarghese@gmail.com>
2024-01-10 18:49:42 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -4113,8 +4113,10 @@ function hoverInit(e) { // desktop hover effect
 }
 
 function hoverEnd(e) {
-    const mouseY = e.clientY;
-    const mouseX = e.clientX;
+    if (e) {
+        const mouseY = e.clientY;
+        const mouseX = e.clientX;
+    }
 
     cursorBig.classList.remove("extra");
 
