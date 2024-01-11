5fead8b3964e1a4da0071563b1aca82f86b1a89f
ivansojivarghese <ivansojivarghese@gmail.com>
2024-01-11 12:04:55 +0800

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -135,10 +135,12 @@ function fetchPWAInfo() {
             wordcloud[i].style.color = "rgb(" + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ")";
 
             wd = getBd(wordcloud[i], "width");
-            if (wd > ((0.9 * wiD) - (2 * op.fN))) {
-                wordcloud[i].style.display = "none";
-                wordcloud[i].remove();
-            }
+            setTimeout(function() {
+                if (wd > ((0.9 * wiD) - op.fN)) {
+                    wordcloud[i].style.display = "none";
+                    wordcloud[i].remove();
+                }
+            }, 10);
         } else {
             wordcloud[i].style.display = "none";
             wordcloud[i].remove();
