d4fe69ec959f56474156585cd1c37ac77183d00b
ivansojivarghese <ivansojivarghese@gmail.com>
2024-01-21 21:07:53 +0800

    Update ld.js

--- js/ld.js
+++ js/ld.js
@@ -129,13 +129,13 @@ function docRead() {
             if (op.pwa.s) {
                 // const pwa_css_link = document.querySelector('#pwa_css_link');
                 // pwa_css_link.href = "css/pwa.css";
-
+                /*
                 deleteAllCookies();
 
                 const puller = document.querySelector('.puller');
                 puller.style.top = "2rem";
                 e_Fd(puller, false);
-                endRefresh();
+                endRefresh();*/
 
                 rL.e4 = true;
             } else {
@@ -759,13 +759,12 @@ function docRead() {
 
                 c_css("#scrollBar", "right: 0.2rem; width: 0.2rem;", false, null);
 
-                /*
                 deleteAllCookies();
 
                 const puller = document.querySelector('.puller');
                 puller.style.top = "2rem";
                 e_Fd(puller, false);
-                endRefresh();*/
+                endRefresh();
 
                 clientAPI();
                 countryAPI("");
