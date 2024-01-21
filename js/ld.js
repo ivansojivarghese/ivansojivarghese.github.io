723ebcdf2a1c27401de7df8053a9eaa46d4f2200
ivansojivarghese <ivansojivarghese@gmail.com>
2024-01-21 17:25:23 +0800

    Update ld.js

--- js/ld.js
+++ js/ld.js
@@ -931,7 +931,7 @@ function docRead() {
                 }, op.t);
 
                 normal_body.classList.add("d_n");
-                document.title = "Clicks, Code & Concepts";
+                document.title = "Ivan Varghese";
 
                 rL.i = true; // end load
                 rL.s = true;
