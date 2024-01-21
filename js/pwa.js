857c0c75900cd51c32177479062b6164910ef450
ivansojivarghese <ivansojivarghese@gmail.com>
2024-01-21 19:43:36 +0800

    update

--- js/pwa.js
+++ js/pwa.js
@@ -316,22 +316,22 @@ function updateBatteryUI(battery) {
     if (btty.charging) { // charging
         batteryIcon.classList.add("battery_chg_img");
     } else if (btty.level > 87.5) { // full
-        var img = (!op.darkMode || init) ? "battery_full_img" : "";
+        var img = (!op.darkMode || init) ? "battery_full_img" : "battery_full_w_img";
         batteryIcon.classList.add(img);
     } else if (btty.level > 75) { // 6
-        var img = (!op.darkMode || init) ? "battery_6_img" : "";
+        var img = (!op.darkMode || init) ? "battery_6_img" : "battery_6_w_img";
         batteryIcon.classList.add(img);
     } else if (btty.level > 62.5) { // 5
-        var img = (!op.darkMode || init) ? "battery_5_img" : "";
+        var img = (!op.darkMode || init) ? "battery_5_img" : "battery_5_w_img";
         batteryIcon.classList.add(img);
     } else if (btty.level > 50) { // 4
-        var img = (!op.darkMode || init) ? "battery_4_img" : "";
+        var img = (!op.darkMode || init) ? "battery_4_img" : "battery_4_w_img";
         batteryIcon.classList.add(img);
     } else if (btty.level > 37.5) { // 3
-        var img = (!op.darkMode || init) ? "battery_3_img" : "";
+        var img = (!op.darkMode || init) ? "battery_3_img" : "battery_3_w_img";
         batteryIcon.classList.add(img);
     } else if (btty.level > 25) { // 2
-        var img = (!op.darkMode || init) ? "battery_2_img" : "";
+        var img = (!op.darkMode || init) ? "battery_2_img" : "battery_2_w_img";
         batteryIcon.classList.add(img);
     } else if (btty.level > 12.5) { // 1
         batteryIcon.classList.add("battery_1_img");
