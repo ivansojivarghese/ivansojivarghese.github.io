b39631f1209765474cd0383fe704c3b47056850e
ivansojivarghese <ivansojivarghese@gmail.com>
2024-01-23 01:29:02 +0800

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -43,6 +43,11 @@ function navButtonActive(b, e, v) {
     var target = e.currentTarget || e;
     const buttons = document.querySelectorAll('.pwa .navbar .button');
 
+    if (target.classList.contains("about")) {
+        const profile_image = document.querySelector('.pwa .profile_image');
+        e_Fd(profile_image, false);
+    }
+
     for (i = 0; i < buttons.length; i++) { // remove from other non-targets
         if (buttons[i] !== target) {
             if ((buttons[i].classList.contains("buttonActive") && !op.darkChange) || (!buttons[i].classList.contains("buttonActive") && op.darkChange)) {
