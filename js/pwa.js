fa3f10599d58b75cb32d77dac406fcc5d9943eae
ivansojivarghese <ivansojivarghese@gmail.com>
2024-03-14 07:13:07 +0800

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -352,7 +352,7 @@ function fetchPWAInfo() {
         if (op.darkMode) { // if dark mode
             toggleColorMode(null, true); // start-up with preset color theme
         } else {
-            autoDarkMode(); // EXPERIMENTAL: Check device ambient light to activate dark mode
+            // autoDarkMode(); // EXPERIMENTAL: Check device ambient light to activate dark mode
         }
     } else {
         if (getCookie("darkMode") === "true") { // manual: dark mode
