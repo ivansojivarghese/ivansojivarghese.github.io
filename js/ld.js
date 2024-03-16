d7b855174c106b37649e392e41841bad249f9742
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-03-16 19:06:57 +0800

    Update ld.js

--- js/ld.js
+++ js/ld.js
@@ -2209,7 +2209,7 @@ function autoDarkMode() {
                 }
 
                 var daytime = false;
-                /*
+                
                 if (!op.pwa.s) {
                     daytime = checkWithinTime(timeToDetails(sunAPIres.sunrise), timeToDetails(sunAPIres.sunset)); // check if user time is daytime
                 } else {
@@ -2221,7 +2221,7 @@ function autoDarkMode() {
                     } else {
                         daytime = false;
                     }
-                }*/
+                }
 
                 // Read the light levels in lux 
                 // < 50 is dark room
