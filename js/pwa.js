0b2a2dbcec217559496c1cc42d0a49bacf7768c2
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-03-17 15:02:19 +0800

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -329,7 +329,6 @@ window.addEventListener('devicemotion', function(event) { // estimate walking st
         yVal = "",
         stepIncr = true,
         absXVal = Math.abs(Math.round(event.acceleration.x)),
-        // absZGravity = Math.abs(event.accelerationIncludingGravity.z),
         preZVal = acceleration.z[acceleration.z.length - 1],
         preYVal = acceleration.y[acceleration.y.length - 1];
 
@@ -439,7 +438,6 @@ window.addEventListener('devicemotion', function(event) { // estimate walking st
         if ((stepIncr) && (!Math.round(event.acceleration.y) && !Math.round(event.acceleration.z && !Math.round(event.acceleration.x))) && (Math.abs(Math.round(event.rotationRate.alpha)) <= 45) && (Math.abs(Math.round(event.rotationRate.beta)) <= 45) && (Math.abs(Math.round(event.rotationRate.gamma)) <= 90)) {
             clearInterval(motionInterval);
             stepsCount++;
-            steps.innerHTML = "steps: " + stepsCount;
             for (const x in stepsPatternZ) {
                 stepsPatternZ[x] = 0;
             }
@@ -456,6 +454,7 @@ window.addEventListener('devicemotion', function(event) { // estimate walking st
             }, 1500);
         }
 
+        steps.innerHTML = "steps: " + stepsCount;
         speedX.innerHTML = motion;
     } 
 
