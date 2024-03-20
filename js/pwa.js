74b05d0c5e00ba03d22bbd94b848df730d6c2e03
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-03-20 19:25:03 +0800

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -530,7 +530,7 @@ window.addEventListener('devicemotion', function(event) { // estimate walking st
             velocity.innerHTML = "velocity: " + velocityEst.toFixed(1) + " " + velocityUnit; 
         }
 
-        speedX.style.backgroundColor = "chocolate";
+        speedX.style.backgroundColor = "red";
         speedX.style.color = "white";
 
             /*
