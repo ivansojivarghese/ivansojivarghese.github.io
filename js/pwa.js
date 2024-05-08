62a5df1deb6191ec1ccde952e49da422222829b0
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-05-08 17:14:01 +0800

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -265,7 +265,7 @@ function navButtonActive(b, e, v) {
             }
             fab2.addEventListener("click", fab2Action);
         }
-    } else if (fab2Check && 'share' in navigator) {
+    } else if (!fab2Check || (fab2Check && b === "about" && 'share' in navigator)) {
         fab.classList.add("hide");
         if (fab2Check) {
             fab2.classList.add("hide");
