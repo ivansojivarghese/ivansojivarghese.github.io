4473c32fb7a2aa24167eee3e143489220e936f86
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-05-09 08:30:44 +0800

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -286,6 +286,11 @@ function navButtonActive(b, e, v) {
                 fab2Close = true;
             }
             fab2.addEventListener("click", fab2Action);
+        } else {
+            fab2i.classList.remove("home_w_img");
+            fab2i.classList.add("close_w_img");
+            fab2.style.backgroundColor = "var(--negate_col)";
+            fab2Close = true;
         }
     } else if (!fab2Check || (fab2Check && b !== "about" && b !== "home")) {
         fab.classList.add("hide");
