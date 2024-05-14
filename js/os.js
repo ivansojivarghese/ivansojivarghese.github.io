1339927042628512f05d926634d23e614c58174d
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-05-15 01:48:16 +0800

    Update os.js

--- js/os.js
+++ js/os.js
@@ -90,7 +90,9 @@ function toggleColorMode(e, init) { // light/dark modes toggling
             fvc_d = ["safari-pinned-tab-dark.svg", "apple-touch-icon_dark.png", "favicon_dark.ico", "favicon/favicon_dark.svg", "favicon/android-chrome-512x512_dark.png", "favicon/android-chrome-192x192_dark.png", "favicon/favicon-32x32_dark.png", "favicon/favicon-16x16_dark.png"]; // dark favicons
             fvc_L = ["safari-pinned-tab.svg", "apple-touch-icon.png", "favicon.ico", "favicon/favicon.svg", "favicon/android-chrome-512x512.png", "favicon/android-chrome-192x192.png", "favicon/favicon-32x32.png", "favicon/favicon-16x16.png"]; // light favicons
 
-        colorStates++;
+        if (!init) {
+            colorStates++;
+        }
 
         if (e) {
             op.darkChange = true;
