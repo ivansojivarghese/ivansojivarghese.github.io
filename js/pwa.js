51ff4aee95a09f9d37ab3c51cfe8958512dfd338
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-05-14 20:45:22 +0800

    update

--- js/pwa.js
+++ js/pwa.js
@@ -1485,7 +1485,7 @@ function fetchPWAInfo() {
             // rotationLockToggle.classList.add("toggleOn");
             // screen.orientation.lock("any");
 
-            oriLock = screen.orientation.lock('natural').then(() => {
+            oriLock = screen.orientation.lock(screen.orientation.type).then(() => {
                 rotationLockToggle.classList.add('toggleOn');
                 localStorage.setItem('rotationLock', '1');
                 toggles.rotationLock = 1;
