9810a04049910f700669c24869d46842d246456f
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-05-14 20:12:47 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -4011,6 +4011,9 @@ window.addEventListener("touchstart", function(event) { // IF TOUCH detected on 
         tch.d = false;
         tch.yA = event.touches[0].clientY;
     }
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+        document.documentElement.requestFullscreen();
+    }
 });
 
 window.addEventListener("touchmove", function(event) {
