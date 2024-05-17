ca5cd53b30b7b68f588203e0594e01c4e92cca7d
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-05-17 11:44:12 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -4205,7 +4205,7 @@ function hoverMiddle(e) {
     const mouseY = e.clientY;
     const mouseX = e.clientX;
 
-    if (e.currentTarget.getAttribute("onclick") === 'toggleColorMode(event)' || e.currentTarget.classList.contains("toggleBackg")) {
+    if (e.currentTarget.getAttribute("onclick") === 'toggleColorMode(event)' || (e.currentTarget.classList.contains("toggleBackg") && !e.currentTarget.classList.contains("hide"))) {
 
         cursorBig.style.transform = `translate3d(calc(${mouseX}px - 0.7rem), calc(${mouseY}px - 0.7rem), 0)`;
 
