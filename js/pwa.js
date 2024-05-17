eebec68e1af6e26acc0fe0b56cf46171530dd9a1
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-05-18 00:20:18 +0800

    update

--- js/pwa.js
+++ js/pwa.js
@@ -2230,6 +2230,8 @@ function loadError(input) {
     const load_m = document.querySelector("#load_message");
     const load_txt = document.querySelector("#load_message h3");
 
+    c_css(".darkText", "color: #FFF !important;", false, null, op, "darkMode");
+
     rL.r_s = false;
     rL.el = document.getElementById("load_sc"), 
     rL.r = document.getElementById("loadR"); // loading rings (container)
