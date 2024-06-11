53c9c4972349298b51adbd271e96d344f9756746
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-06-12 01:00:33 +0800

    Update msc.js

--- js/msc.js
+++ js/msc.js
@@ -329,6 +329,7 @@ async function ipAPI(v) {  // Free usage, unlimited, https://www.findip.net/
                 });
             })
     } else {
+        ipAPIres.country = {};
         ipAPIres.country.iso_code = isolateRegionBCP47(navigator.language);
         ipAPIres.error = true;
     }
