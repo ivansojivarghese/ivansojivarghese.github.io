6156b612427d38ef3eef442b9ce7ac5429505cca
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-06-15 15:45:25 +0800

    Update repos.js

--- js/repos.js
+++ js/repos.js
@@ -1,11 +1,11 @@
 
 import { Octokit } from "https://esm.sh/@octokit/rest";
 
-var halfauth = "4qmTdseGq1IXK3DKmIR";
+var auth = "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";
 
 // const octokit = new Octokit(); // REFERENCE: https://github.com/octokit/rest.js
 const octokit = new Octokit({
-    auth: 'ghp_Jg1LQBL1DrsOE9cRc' + halfauth // PERSONAL ACCESS TOKEN
+    auth: '' // PERSONAL ACCESS TOKEN
 });
 
 // Compare: https://docs.github.com/en/rest/reference/repos/#list-organization-repositories
