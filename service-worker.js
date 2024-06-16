823eb0a21b59b24e8ff557301995ccaec4cb539f
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-06-16 23:48:46 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -285,7 +285,23 @@ self.addEventListener("fetch", (event) => {
 
 self.addEventListener("notificationclick", (event) => {
 	if (event.notification.tag === "update") {
+		//For root applications: just change "'./'" to "'/'"
+		//Very important having the last forward slash on "new URL('./', location)..."
+		const rootUrl = new URL('./', location).href; 
+		event.waitUntil(
+			clients.matchAll().then(matchedClients =>
+			{
+				for (let client of matchedClients)
+				{
+					if (client.url.indexOf(rootUrl) >= 0)
+					{
+						return client.focus();
+					}
+				}
 
+				return clients.openWindow(rootUrl).then(function (client) { client.focus(); });
+			})
+		);
 	}
 	event.notification.close();
 });
