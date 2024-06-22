feea5b3326e14f96507fa0690402b5702c69031f
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-06-22 18:13:21 +0800

    Revert "update"
    This reverts commit 4bf3c4d3e24ce722e33f2d00e1fe4463da97b29f.

--- service-worker.js
+++ service-worker.js
@@ -245,15 +245,6 @@ self.addEventListener("fetch", (event) => {
 
 });
 
-self.addEventListener('push', ev => {
-	const data = ev.data.json();
-	console.log('Got push', data);
-	self.registration.showNotification(data.title, {
-	  body: 'Hello, World!',
-	  icon: 'http://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png'
-	});
-});
-
 self.addEventListener('periodicsync', (event) => {
 	if (event.tag === 'content-sync') {
 		event.waitUntil(doSync()); //
