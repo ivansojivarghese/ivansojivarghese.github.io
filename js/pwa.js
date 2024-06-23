dbf826ff6067552fc23ff662506e7201ca9f93a5
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-06-23 19:44:14 +0800

    update

--- js/pwa.js
+++ js/pwa.js
@@ -1504,6 +1504,24 @@ function showUpdateAvailable(m) { //
     }
 }
 
+// Web-Push
+// Public base64 to Uint
+// REFERENCE: https://gist.github.com/Klerith/80abd742d726dd587f4bd5d6a0ab26b6 
+function urlBase64ToUint8Array(base64String) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
 var registration = null;
 
 async function periodicSync() { //
