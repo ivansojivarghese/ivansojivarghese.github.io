6caa0d6a51dd69a26da237d201c80b6249608493
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-06-23 16:04:54 +0800

    update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,21 +3,6 @@
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
-const UrlFirebaseConfig = new URLSearchParams(
-    {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL ,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID  ,
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ,
-      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ,
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ,
-      measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ,
-    }.toString()
-  );
-
-  const swUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}/firebase-messaging-sw.js?${UrlFirebaseConfig}`;
-  
 
 // Set Firebase configuration, once available
 self.addEventListener('fetch', () => {
