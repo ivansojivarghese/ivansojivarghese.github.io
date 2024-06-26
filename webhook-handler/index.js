4f3d2f57b5dd75d0de1677501fd45015ebcf6d64
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-06-26 12:07:59 +0800

    update

--- webhook-handler/index.js
+++ webhook-handler/index.js
@@ -3,6 +3,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Import cors middleware
 
 const app = express();
 const port = process.env.PORT || 3000;
