4bd2e5867ff04f6a8a13d1e7b7ea8e4af6c6f482
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-08-15 12:44:30 +0800

    update

---
+++ media-player/network.js
@@ -0,0 +1,22 @@
+
+// network 
+
+const fileSize = 5301699; // resource file size (in bytes)
+const speed = 0;
+
+const estimateNetworkSpeed = async() => { // estimate network speed
+    try {
+        var s;
+        var startTime = new Date().getTime(); // start time of fetch
+        const online = await fetch("https://ivansojivarghese.github.io/media-player/msc/networkSpeedEstimator.jpg", { // send a 'ping' signal to resource locator
+            cache : "no-store",
+            priority: "low"
+        });
+        var endTime = new Date().getTime(); // end time of fetch
+        speed = (fileSize / ((endTime - startTime) / 1000)) / 1000000; // approx. network speed (in MBps)
+        
+    } catch (err) { // if network error
+        speed = 0; // return 0 mbps
+        // return true; // default true
+    }
+}
