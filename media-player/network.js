3fc724683bfc7c5292111ae68cc6d0effb845dbe
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-08-23 10:41:33 +0800

    Update network.js

--- media-player/network.js
+++ media-player/network.js
@@ -4,6 +4,13 @@
 const fileSize = 5301699; // resource file size (in bytes)
 var networkSpeed = 0;
 
+var rtt = 0,
+    downlink = 0,
+    downlinkMax = 0,
+    droppedFrames = 0,
+    effectiveType = "",
+    saveData = null;
+
 const estimateNetworkSpeed = async() => { // estimate network speed
     try {
         var startTime = new Date().getTime(); // start time of fetch
@@ -21,3 +28,15 @@ const estimateNetworkSpeed = async() => { // estimate network speed
 }
 
 estimateNetworkSpeed();
+
+function getNetworkInfo() {
+    rtt = navigator.connection.rtt;
+    downlink = navigator.connection.downlink;
+    downlinkMax = navigator.connection.downlinkMax;
+    effectiveType = navigator.connection.effectiveType;
+    saveData = navigator.connection.saveData;
+}
+
+getNetworkInfo();
+
+navigator.connection.addEventListener('change', getNetworkInfo);
