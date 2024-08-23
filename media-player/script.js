0375b54eee7816f2d06b62b987ad5409868a158a
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-08-23 16:30:09 +0800

    Update script.js

--- media-player/script.js
+++ media-player/script.js
@@ -37,6 +37,46 @@
 
     var interactiveType = "";
 
+    // REFERENCE: https://stackoverflow.com/questions/21399872/how-to-detect-whether-html5-video-has-paused-for-buffering
+    var checkInterval  = 50.0 // check every 50 ms (do not use lower values)
+    var lastPlayPos    = 0
+    var currentPlayPos = 0
+    var bufferingDetected = false
+    // var player = document.getElementById('videoPlayer')
+
+    setInterval(checkBuffering, checkInterval);
+    function checkBuffering() {
+        currentPlayPos = player.currentTime
+
+        // checking offset should be at most the check interval
+        // but allow for some margin
+        var offset = (checkInterval - 20) / 1000
+
+        // if no buffering is currently detected,
+        // and the position does not seem to increase
+        // and the player isn't manually paused...
+        if (
+                !bufferingDetected 
+                && currentPlayPos < (lastPlayPos + offset)
+                && !player.paused
+            ) {
+            console.log("buffering")
+            bufferingDetected = true
+        }
+
+        // if we were buffering but the player has advanced,
+        // then there is no buffering
+        if (
+            bufferingDetected 
+            && currentPlayPos > (lastPlayPos + offset)
+            && !player.paused
+            ) {
+            console.log("not buffering anymore")
+            bufferingDetected = false
+        }
+        lastPlayPos = currentPlayPos
+    }
+
     function updatePositionState() {
       if ('setPositionState' in navigator.mediaSession) {
         navigator.mediaSession.setPositionState({
