ffd44a0eb98073d2406c0aa5a248d4f1087209fd
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-10-08 16:53:16 +0800

    update

--- media-player/js/msc.js
+++ media-player/js/msc.js
@@ -26,3 +26,28 @@
     }
 
     // getScreenLock();
+
+    window['__onGCastApiAvailable'] = function(isAvailable) {
+        if (isAvailable) {
+          initializeCastApi();
+        }
+      };
+      
+      function initializeCastApi() {
+        cast.framework.CastContext.getInstance().setOptions({
+          receiverApplicationId: chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID,
+          autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
+        });
+      }
+
+      function castMedia() {
+        const session = cast.framework.CastContext.getInstance().getCurrentSession();
+        const mediaInfo = new chrome.cast.media.MediaInfo('VIDEO_URL_HERE', 'video/mp4');
+        const request = new chrome.cast.media.LoadRequest(mediaInfo);
+        session.loadMedia(request).then(
+          () => console.log('Media loaded successfully'),
+          (error) => console.log('Error loading media', error)
+        );
+      }
+      
+      document.getElementById('castButton').addEventListener('click', castMedia);
