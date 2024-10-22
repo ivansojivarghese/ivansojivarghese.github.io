0bb00cd047afce21d648ef388514610772c73497
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-10-22 16:18:51 +0800

    Update network.js

--- media-player/js/network.js
+++ media-player/js/network.js
@@ -16,6 +16,8 @@ var networkSpeedInt = null;
 var networkQuality = "";
 var networkQualityRange = 0;
 
+var networkParamInt = null;
+
 var rtt = 0,
     downlink = 0,
     downlinkMax = 0,
@@ -121,6 +123,12 @@ const estimateNetworkSpeed = async() => { // estimate network speed
 
 if (networkSpeedInt === null) {
     networkSpeedInt = setInterval(estimateNetworkSpeed, networkIntRange); 
+}
+if (networkParamInt === null) {
+    networkParamInt = setInterval(function() {
+        measureJitter(pingsCount, 1000);
+        measurePacketLoss(testFileUrl);
+    }, 60000); 
 }
 
 function timeToSeconds(txt) { // REFERENCE: https://javascript.plainenglish.io/how-to-convert-hh-mm-ss-time-string-to-seconds-only-in-javascript-e11a0a4726d2#:~:text=Convert%20HH%3AMM%3ASS%20Time%20String%20to%20Seconds%20Only%20in%20JavaScript,-We%20can%20convert&text=const%20hms%20%3D%20'02%3A04,)%20*%2060%20%2B%20(%2Bseconds)%3B
