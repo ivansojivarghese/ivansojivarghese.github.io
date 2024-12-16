549148b0901f9c8b9414866034f66fe01b0c9a10
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-12-16 10:03:37 +0800

    Update script.js

--- media-player/js/script.js
+++ media-player/js/script.js
@@ -4467,7 +4467,7 @@
 
                 // Calculate an activity score
                 // const activityScore = (frameChange / timeDiff) * complexity;
-                const activityScore = (frameChange / timeDiff) * (complexity * 50);
+                const activityScore = (frameChange / timeDiff) * (complexity * 100);
 
                 // Log activity score per frame
                 console.log(`Activity Score: ${activityScore.toFixed(3)}`);
