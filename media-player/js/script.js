3622626ff19d3f22d740ba8b3aaf55b0d6711e03
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-12-24 18:07:52 +0800

    Update script.js

--- media-player/js/script.js
+++ media-player/js/script.js
@@ -1250,7 +1250,7 @@
 
       var ori = screen.orientation.type;
 
-      if (video.src !== "" && videoControls.classList.contains('visible') && (!loading || initialVideoLoad) && (!videoErr && !audioErr)) {
+      if (video.src !== "" && videoControls.classList.contains('visible') && ((!loading || loading) || initialVideoLoad) && (!videoErr && !audioErr)) {
         videoInfoElm.info.style.transform = "none";
         videoInfoOpen = true;
 
