053e68a0627ebee16f33b1eec8507216d47b8eae
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2024-12-28 22:29:40 +0800

    update

--- media-player/js/video.js
+++ media-player/js/video.js
@@ -17,7 +17,6 @@ var videoInfoElm = {
   replay : document.querySelector("#infoContainer div.replay"),
   cast : document.querySelector("#infoContainer div.cast"),
   gCast : document.querySelector("google-cast-launcher"),
-  gCastPath : document.querySelectorAll("google-cast-launcher path")[0],
 
   autoResBtn : document.querySelector("#infoContainer div.autoResBtn"),
   autoResLive : document.querySelector("#infoContainer p.autoResLive"),
@@ -2147,10 +2146,10 @@ setInterval(() => {
   } else {
     videoInfoElm.cast.style.display = "none";
   }
-
+  /*
   if (videoInfoElm.gCastPath.classList.contains("cast_caf_state_c")) {
     videoInfoElm.cast.classList.add("active");
   } else {
     videoInfoElm.cast.classList.remove("active");
-  }
+  }*/
 }, 1000/60);
