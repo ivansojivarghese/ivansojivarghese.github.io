81149980a056da2fac7068e4642d6b809df9923f
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2025-01-13 16:10:55 +0800

    Update request.js

--- media-player/js/request.js
+++ media-player/js/request.js
@@ -6,6 +6,12 @@
     const searchOptions = document.querySelector(".searchOptions");
 
     const pattern = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)(\/.*)?$/;
+
+    var queryType = document.querySelector("#searchType"),
+        queryDate = document.querySelector("#searchUpload"),
+        querySort = document.querySelector("#searchSort");
+
+    var searchResults;
 /*
     inp.addEventListener('select', function() {
       this.selectionStart = this.selectionEnd;
@@ -13,76 +19,42 @@
     
     function getURL(u) {
 
-      if ((inp.value !== "" && (pattern.test(inp.value) || searchPath === "query")) || (u)) {
+      if (searchPath === "url") {
+        if ((inp.value !== "" && (pattern.test(inp.value) && searchPath === "url")) || (u)) {
 
-        var wrapURL = document.querySelector("#urlInput");
-        var wrap = document.querySelector("#settingsOptions");
-        var contents = inp.value || u;
+          var wrapURL = document.querySelector("#urlInput");
+          var wrap = document.querySelector("#settingsOptions");
+          var contents = inp.value || u;
 
-        if (u) {
-          closeVideoInfo();
-        }
-
-        if (contents !== "") {
-          videoURL = contents;
-        }
-
-        var error = false;
-        
-        // NO ACCESS TO SHORTS, LIVE, ATTRIBUTED OR EMBEDDED VIDEOS
-        if (!contents.includes("embed") && !contents.includes("attribution_link") && !contents.includes("shorts") && !contents.includes("live")) {
-          
-        if (contents.includes("youtube.com") && !contents.includes("m.youtube.com")) {
-          
-          //video.classList.remove("error");
-          // video.style.border = "0.15rem solid #A10000";
-          
-          var sIndex = contents.indexOf("v=");
-          var eIndex = contents.indexOf("&");
-          var sTrue;
-          
-          if (sIndex !== -1) {
-            sTrue = true;
-            if (eIndex === -1 && eIndex > sIndex) {
-              var eIndex = contents.indexOf("#");
-                if (eIndex === -1) {
-                  eIndex = contents.length;
-                }
-            } else if (eIndex < sIndex) {
-              eIndex = contents.length;
-            }
-          } else {
-            sTrue = false;
-            sIndex = contents.lastIndexOf("/");
-            eIndex = contents.indexOf("?");
-            if (eIndex === -1) {
-              eIndex = contents.length;
-            }
+          if (u) {
+            closeVideoInfo();
           }
-          
-          var id = contents.substring(sIndex, eIndex);
-          if (sTrue) {
-            var videoID = id.substring(2, id.length);
-          } else {
-            var videoID = id.substring(1, id.length);
+
+          if (contents !== "") {
+            videoURL = contents;
           }
+
+          var error = false;
           
-        } else if (contents.includes("m.youtube.com")) {
-          
-            // video.classList.remove("error");
-          // video.style.border = "0.15rem solid #A10000";
+          // NO ACCESS TO SHORTS, LIVE, ATTRIBUTED OR EMBEDDED VIDEOS
+          if (!contents.includes("embed") && !contents.includes("attribution_link") && !contents.includes("shorts") && !contents.includes("live")) {
+            
+          if (contents.includes("youtube.com") && !contents.includes("m.youtube.com")) {
+            
+            //video.classList.remove("error");
+            // video.style.border = "0.15rem solid #A10000";
             
             var sIndex = contents.indexOf("v=");
             var eIndex = contents.indexOf("&");
             var sTrue;
-          
+            
             if (sIndex !== -1) {
               sTrue = true;
               if (eIndex === -1 && eIndex > sIndex) {
                 var eIndex = contents.indexOf("#");
-                if (eIndex === -1) {
-                  eIndex = contents.length;
-                }
+                  if (eIndex === -1) {
+                    eIndex = contents.length;
+                  }
               } else if (eIndex < sIndex) {
                 eIndex = contents.length;
               }
@@ -94,52 +66,161 @@
                 eIndex = contents.length;
               }
             }
-          
+            
             var id = contents.substring(sIndex, eIndex);
             if (sTrue) {
               var videoID = id.substring(2, id.length);
             } else {
               var videoID = id.substring(1, id.length);
             }
+            
+          } else if (contents.includes("m.youtube.com")) {
+            
+              // video.classList.remove("error");
+            // video.style.border = "0.15rem solid #A10000";
               
-        } else if (contents.includes("youtu.be")) {
-          
-          // video.classList.remove("error");
-          // video.style.border = "0.15rem solid #A10000";
-          
-          var sIndex = contents.indexOf("e/");
-            var eIndex = contents.indexOf("?");
-          var sTrue;
-          
-          if (sIndex === -1) {
-            sTrue = false;
-            sIndex = contents.lastIndexOf("/");
-            eIndex = contents.indexOf("?");
-            if (eIndex === -1) {
-              eIndex = contents.indexOf("&");
+              var sIndex = contents.indexOf("v=");
+              var eIndex = contents.indexOf("&");
+              var sTrue;
+            
+              if (sIndex !== -1) {
+                sTrue = true;
+                if (eIndex === -1 && eIndex > sIndex) {
+                  var eIndex = contents.indexOf("#");
+                  if (eIndex === -1) {
+                    eIndex = contents.length;
+                  }
+                } else if (eIndex < sIndex) {
+                  eIndex = contents.length;
+                }
+              } else {
+                sTrue = false;
+                sIndex = contents.lastIndexOf("/");
+                eIndex = contents.indexOf("?");
+                if (eIndex === -1) {
+                  eIndex = contents.length;
+                }
+              }
+            
+              var id = contents.substring(sIndex, eIndex);
+              if (sTrue) {
+                var videoID = id.substring(2, id.length);
+              } else {
+                var videoID = id.substring(1, id.length);
+              }
+                
+          } else if (contents.includes("youtu.be")) {
+            
+            // video.classList.remove("error");
+            // video.style.border = "0.15rem solid #A10000";
+            
+            var sIndex = contents.indexOf("e/");
+              var eIndex = contents.indexOf("?");
+            var sTrue;
+            
+            if (sIndex === -1) {
+              sTrue = false;
+              sIndex = contents.lastIndexOf("/");
+              eIndex = contents.indexOf("?");
               if (eIndex === -1) {
-                eIndex = contents.length;
+                eIndex = contents.indexOf("&");
+                if (eIndex === -1) {
+                  eIndex = contents.length;
+                }
+              }
+            } else {
+              sTrue = true;
+              if (eIndex === -1) {
+                eIndex = contents.indexOf("&");
+                if (eIndex === -1) {
+                  eIndex = contents.length;
+                }
               }
             }
-          } else {
-            sTrue = true;
-            if (eIndex === -1) {
-              eIndex = contents.indexOf("&");
-              if (eIndex === -1) {
-                eIndex = contents.length;
-              }
+            
+            var id = contents.substring(sIndex, eIndex);
+            if (sTrue) {
+              var videoID = id.substring(2, id.length);
+            } else {
+              var videoID = id.substring(1, id.length);
             }
-          }
-          
-          var id = contents.substring(sIndex, eIndex);
-          if (sTrue) {
-            var videoID = id.substring(2, id.length);
           } else {
-            var videoID = id.substring(1, id.length);
-          }
-        } else {
 
-          error = true;
+            error = true;
+            
+            statusIndicator.classList.remove("buffer");
+            statusIndicator.classList.remove("smooth");
+            statusIndicator.classList.add("error");
+
+            endLoad();
+                      
+            setTimeout(function() {
+              loadingRing.style.display = "none";
+              playPauseButton.style.display = "block";
+              playPauseButton.classList.remove('playing');
+              playPauseButton.title = "Play";
+
+              if (!seekingLoad && !longTap && !seeking) {
+                hideVideoControls();
+              }
+
+              // reset the loader
+              setTimeout(function() {
+                resetLoad();
+              }, 10);
+
+            }, 1000);
+
+            loading = false;
+
+          }
+
+          /*
+          let VID_REGEX =
+          /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+          videoID = contents.match(VID_REGEX)[1];*/
+
+          if (!error) {
+
+            var timestamp = Number(contents.slice(contents.indexOf("&t=") + 3)); // START FROM (in sec.)
+            if (!timestamp && (videoURL === localStorage.getItem("mediaURL"))) {
+              timestamp = Number(localStorage.getItem("timestamp"));
+            }
+          
+            console.log("Video ID: " + videoID);
+              
+            videoSubmit = true;
+            
+            // if (videoID) {
+            if (videoLoadLoop === null) {
+              
+              videoLoadLoop = setInterval(() => {
+
+                if (countryAPIres.online) {
+                  clearInterval(videoLoadLoop);
+                  videoLoadLoop = null;
+
+                  // if (failTimes < maxFailTimes) {
+                    getParams(videoID, timestamp);
+                  // }
+                }
+
+              }, 100);
+            }
+            
+            // }
+
+          }
+            
+          wrapURL.style.display = "";
+          wrap.style.display = "";
+          inp.value = "";
+
+          if (searchPath === "url") {
+            closeVideoInfo();
+          }
+            
+        } else {
           
           statusIndicator.classList.remove("buffer");
           statusIndicator.classList.remove("smooth");
@@ -165,86 +246,16 @@
           }, 1000);
 
           loading = false;
-
         }
-
-        /*
-        let VID_REGEX =
-        /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
-        videoID = contents.match(VID_REGEX)[1];*/
-
-        if (!error) {
-
-          var timestamp = Number(contents.slice(contents.indexOf("&t=") + 3)); // START FROM (in sec.)
-          if (!timestamp && (videoURL === localStorage.getItem("mediaURL"))) {
-            timestamp = Number(localStorage.getItem("timestamp"));
-          }
         
-          console.log("Video ID: " + videoID);
-            
-          videoSubmit = true;
-          
-          // if (videoID) {
-          if (videoLoadLoop === null) {
-            
-            videoLoadLoop = setInterval(() => {
+        // document.getElementById("urlSubmit").click();
+        } else {
 
-              if (countryAPIres.online) {
-                clearInterval(videoLoadLoop);
-                videoLoadLoop = null;
-
-                // if (failTimes < maxFailTimes) {
-                  getParams(videoID, timestamp);
-                // }
-              }
-
-            }, 100);
-          }
-          
-          // }
-
+          // ERROR
         }
-          
-        wrapURL.style.display = "";
-        wrap.style.display = "";
-        inp.value = "";
-
-        if (searchPath === "url") {
-          closeVideoInfo();
-        }
-          
-      } else {
-        
-        statusIndicator.classList.remove("buffer");
-        statusIndicator.classList.remove("smooth");
-        statusIndicator.classList.add("error");
-
-        endLoad();
-                  
-        setTimeout(function() {
-          loadingRing.style.display = "none";
-          playPauseButton.style.display = "block";
-          playPauseButton.classList.remove('playing');
-          playPauseButton.title = "Play";
-
-          if (!seekingLoad && !longTap && !seeking) {
-            hideVideoControls();
-          }
-
-          // reset the loader
-          setTimeout(function() {
-            resetLoad();
-          }, 10);
-
-        }, 1000);
-
-        loading = false;
-      }
-      
-      // document.getElementById("urlSubmit").click();
       } else {
 
-        // ERROR
+
       }
     }
     
@@ -261,6 +272,25 @@
         }
       }
     });
+
+    async function searchQuery(q) {
+      const url = 'https://yt-api.p.rapidapi.com/search?query=' + q + '&geo=' + countryAPIres.country + '&lang=en&type=' + queryType.value + '&upload_date=' + queryDate.value + '&sort_by=' + querySort.value;
+      const options = {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-key': '89ce58ef37msh8e59da617907bbcp1455bajsn66709ef67e50',
+          'x-rapidapi-host': 'yt-api.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await fetch(url, options);
+        searchResults = await response.json();
+        console.log(searchResults);
+      } catch (error) {
+        console.error(error);
+      }
+    }
     
     /*
     inp.addEventListener("keypress", function(event) {
